80ec4de14763f4c453a1b70b4a40c59f1d9efb55
e-kuznetsov <evgeny.kuznetsov@outlook.com>
2019-11-25 18:53:25 +0300

    add remove workers

--- infra/aws/remove_workers.sh
+++ infra/aws/remove_workers.sh
@@ -6,8 +6,7 @@ set -o pipefail
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
-source "$my_dir/definitions"
-
-ENV_FILE="$WORKSPACE/stackrc"
-source $ENV_FILE
-aws ec2 terminate-instances --region $AWS_REGION --instance-ids $instance_id
+for f in $WORKSPACE/stackrc.deploy-platform-*.env; do
+  source $f
+  aws ec2 terminate-instances --region $AWS_REGION --instance-ids $instance_id
+done
