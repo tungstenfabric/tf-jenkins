bc3f8f73cd6824bc54d53c35818d0fe358b9b07c
Andrey Pavlov <andrey.mp@gmail.com>
2019-11-26 13:01:10 +0300

    add env_os for build job

--- jobs/build/run.sh
+++ jobs/build/run.sh
@@ -8,13 +8,17 @@ my_dir="$(dirname $my_file)"
 
 source "$my_dir/definitions"
 
-ENV_FILE="$WORKSPACE/stackrc"
+ENV_FILE="$WORKSPACE/stackrc.$JOB_NAME.env"
 source $ENV_FILE
 
+env|sort
+
+exit 0
+
 rsync tf-dev-env $IP:
-cat <<EOF
+cat <<EOF | ssh $IP
 export $??_REGISTRY=???_REGISTRY
 export ??_TAG=??_TAG
 export DEV_ENV_IMAGE=???
 ??/tf-dev-env/run.sh build
-EOF | ssh $IP
+EOF
