8d84d85f5598b6fd696c109733aef8ae55e992e0
Andrey Pavlov <andrey-mp@yandex.ru>
2019-11-30 16:27:35 +0300

    enable build

--- jobs/build/run.sh
+++ jobs/build/run.sh
@@ -13,8 +13,6 @@ source $ENV_FILE
 
 rsync -a -e "ssh $SSH_OPTIONS" $WORKSPACE/src $IMAGE_SSH_USER@$instance_ip:./
 
-exit 0
-
 echo "INFO: Build started"
 
 cat <<EOF | ssh $SSH_OPTIONS $IMAGE_SSH_USER@$instance_ip
@@ -22,7 +20,7 @@ cat <<EOF | ssh $SSH_OPTIONS $IMAGE_SSH_USER@$instance_ip
 export DEBUG=$DEBUG
 export PATH=\$PATH:/usr/sbin
 cd src/tungstenfabric/tf-dev-env
-./run.sh build
+bash -ex ./run.sh build
 EOF
 
 echo "INFO: Build finished"
