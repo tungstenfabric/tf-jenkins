784869892f2c92b142b92ef6cdd08e992ca7d778
Andrey Pavlov <andrey.mp@gmail.com>
2019-12-04 16:00:40 +0300

    add UT infra

---
+++ jobs/test/unit/run.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -eE
+set -o pipefail
+
+[ "${DEBUG,,}" == "true" ] && set -x
+
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname $my_file)"
+
+source "$my_dir/definitions"
+
+ENV_FILE="$WORKSPACE/stackrc.$JOB_NAME.env"
+source $ENV_FILE
+
+rsync -a -e "ssh $SSH_OPTIONS" $WORKSPACE/src $IMAGE_SSH_USER@$instance_ip:./
+
+echo "INFO: UT started"
+
+cat <<EOF | ssh $SSH_OPTIONS $IMAGE_SSH_USER@$instance_ip
+[ "${DEBUG,,}" == "true" ] && set -x
+export DEBUG=$DEBUG
+export PATH=\$PATH:/usr/sbin
+
+# dont setup own registry
+export CONTRAIL_DEPLOY_REGISTRY=0
+
+export REGISTRY_IP=$REGISTRY_IP
+export REGISTRY_PORT=$REGISTRY_PORT
+export SITE_MIRROR=http://${REGISTRY_IP}/repository
+
+export CONTRAIL_CONTAINER_TAG=$CONTRAIL_CONTAINER_TAG
+
+# to not to bind contrail sources to container
+export CONTRAIL_DIR=""
+
+export IMAGE=$REGISTRY_IP:$REGISTRY_PORT/tf-developer-sandbox
+export DEVENVTAG=$CONTRAIL_CONTAINER_TAG
+
+cd src/tungstenfabric/tf-dev-env
+./run.sh test src/contrail-api-client/api-lib:test
+EOF
+
+result=$?
+if [[ $result != 0 ]] ; then
+  echo "ERROR: Build failed"
+  exit $result
+fi
+echo "INFO: UT finished successfully"
