f54664be5063d6f87e8738c7f24504724971f4a7
Ivan Timofeev <ivanv.timofeev@gmail.com>
2019-12-05 13:54:39 +0300

    Add collect-logs file

---
+++ jobs/fetch/collect_logs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -eE
+set -o pipefail
+set -x
+
+[ "${DEBUG,,}" == "true" ] && set -x
+
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname $my_file)"
+
+source "$my_dir/definitions"
+
+ENV_FILE="$WORKSPACE/stackrc.$JOB_NAME.env"
+source $ENV_FILE
+
+rsync -a -e "ssh $SSH_OPTIONS" $IMAGE_SSH_USER@$instance_ip:./etc/os-release $WORKSPACE/os-release-test
+
+cat $WORKSPACE/os-release-test
+
