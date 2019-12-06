8f6dd849f8b4ef865a560f9ab2824b8374e96f27
Ivan Timofeev <ivanv.timofeev@gmail.com>
2019-12-06 11:33:06 +0300

    k8s manifests run.sh logs

--- jobs/devstack/k8s_manifests/collect_logs.sh
+++ jobs/devstack/k8s_manifests/collect_logs.sh
@@ -12,6 +12,14 @@ ENV_FILE="$WORKSPACE/stackrc.$JOB_NAME.env"
 echo "ORCHESTRATOR=kubernetes" >> "$ENV_FILE"
 source $ENV_FILE
 
+cat <<EOF | ssh $SSH_OPTIONS $IMAGE_SSH_USER@$instance_ip
+[ "${DEBUG,,}" == "true" ] && set -x
+export WORKSPACE=\$HOME
+export DEBUG=$DEBUG
+export PATH=\$PATH:/usr/sbin
+cd src/tungstenfabric/tf-devstack/k8s_manifests
+ORCHESTRATOR=$ORCHESTRATOR ./run.sh logs
+EOF
 
 rsync -a -e "ssh $SSH_OPTIONS" $IMAGE_SSH_USER@$instance_ip:logs.tgz $WORKSPACE/logs.tgz
 
