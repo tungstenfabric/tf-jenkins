cd1c0bb1f700dbc837d147cb724d046a70066f31
Andrey Pavlov <andrey-mp@yandex.ru>
2019-12-07 15:02:26 +0300

    add comment why platform is skipped in os_ansible

--- jobs/devstack/os_ansible/deploy_platform.sh
+++ jobs/devstack/os_ansible/deploy_platform.sh
@@ -14,6 +14,11 @@ source $ENV_FILE
 
 echo 'INFO: Deploy platform for $JOB_NAME'
 
+# target 'platform' tries to fetch_deployer and use code from that container.
+# but for gerrit's check this container will be created in hour in build job.
+# as a workaround we will skip deploy platform here.
+# next solution is to use tf-developer-sandbox image which was created
+# by fetch job (but with different DEPLOYER_DIR)
 exit 0
 
 rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS" $WORKSPACE/src $IMAGE_SSH_USER@$instance_ip:./
