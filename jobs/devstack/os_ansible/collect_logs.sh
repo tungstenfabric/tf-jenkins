e99947cb134c88c3e61d40d05d10f54ec61fa053
Ivan Timofeev <ivanv.timofeev@gmail.com>
2019-12-10 12:11:01 +0300

    Chdir using pushd/popd

--- jobs/devstack/os_ansible/collect_logs.sh
+++ jobs/devstack/os_ansible/collect_logs.sh
@@ -22,13 +22,9 @@ EOF
 
 rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS" $IMAGE_SSH_USER@$instance_ip:logs.tgz $WORKSPACE/logs.tgz
 
-cd $WORKSPACE
+pushd $WORKSPACE
 tar -xzf logs.tgz
-
 ARCH_FOLDER_NAME="$JOB_NAME\_$BUILD_NUMBER"
-
 ssh -i $ARCHIVE_SSH_KEY $SSH_OPTIONS $ARCHIVE_USERNAME@$ARCHIVE_HOST "mkdir -p /var/www/logs/jenkins_logs/$ARCH_FOLDER_NAME"
-
 rsync -a -e "ssh -i $ARCHIVE_SSH_KEY $SSH_OPTIONS" $WORKSPACE/logs $ARCHIVE_USERNAME@$ARCHIVE_HOST:/var/www/logs/jenkins_logs/$ARCH_FOLDER_NAME
-
-cd $OLDPWD
+popd
