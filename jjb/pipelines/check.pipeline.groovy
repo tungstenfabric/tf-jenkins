ddd7d11e2a7bc16c2b5e9efb7eb49f5f4d1a9e33
Ivan Timofeev <ivanv.timofeev@gmail.com>
2019-12-11 12:06:18 +0300

    export LOG_FILEPATH when run from pipeline

--- jjb/pipelines/check.pipeline.groovy
+++ jjb/pipelines/check.pipeline.groovy
@@ -215,6 +215,7 @@ pipeline {
                         export CONF_PLATFORM="${name}"
                         export BUILD_TAG
                         export DEBUG=true
+                        export LOGS_FILE_PATH
                         "$WORKSPACE/src/progmaticlab/tf-jenkins/jobs/devstack/${name}/collect_logs.sh" || /bin/true
                         "$WORKSPACE/src/progmaticlab/tf-jenkins/infra/${SLAVE}/remove_workers.sh"
                       """
