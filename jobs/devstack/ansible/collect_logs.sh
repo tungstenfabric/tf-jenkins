d8dde6dbbc3fd0ed6509a7df1217020111471994
Andrey Pavlov <andrey-mp@yandex.ru>
2019-12-14 23:07:49 -0800

    change env loading in collect_logs

--- jobs/devstack/ansible/collect_logs.sh
+++ jobs/devstack/ansible/collect_logs.sh
@@ -6,10 +6,10 @@ set -o pipefail
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
-source "$my_dir/definitions"
-
 ENV_FILE=${ENV_FILE:-"$WORKSPACE/stackrc.$JOB_NAME.env"}
 source $ENV_FILE
+
+source "$my_dir/definitions"
 
 cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $IMAGE_SSH_USER@$instance_ip
 [ "${DEBUG,,}" == "true" ] && set -x
