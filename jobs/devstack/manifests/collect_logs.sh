d4c0b0658981c591f3d2ac5c39ce7621d795733b
Ivan Timofeev <ivanv.timofeev@gmail.com>
2019-12-16 11:29:58 +0300

    Debug deploy platform logs

--- jobs/devstack/manifests/collect_logs.sh
+++ jobs/devstack/manifests/collect_logs.sh
@@ -20,13 +20,18 @@ echo INFO: Sanity logs path content
 ls -la /home/centos/src/tungstenfabric/tf-test/contrail-sanity/contrail-test-runs/ || /bin/true
 cd src/tungstenfabric/tf-devstack/k8s_manifests
 ORCHESTRATOR=$ORCHESTRATOR ./run.sh logs
+echo INFO: ls current dir
+ls -la
+echo INFO: ls workspace
+ls -la \$WORKSPACE
+
 EOF
 
 rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS" $IMAGE_SSH_USER@$instance_ip:logs.tgz $WORKSPACE/logs.tgz
 
 pushd $WORKSPACE
-tar -xzf logs.tgz
-FULL_LOGS_PATH="${LOGS_PATH}/${JOB_LOGS_PATH}"
+tar -xzf logs.tgz FULL_LOGS_PATH="${LOGS_PATH}/${JOB_LOGS_PATH}"
+ls -la
 ssh -i $LOGS_HOST_SSH_KEY $SSH_OPTIONS $LOGS_HOST_USERNAME@$LOGS_HOST "mkdir -p $FULL_LOGS_PATH"
 rsync -a -e "ssh -i $LOGS_HOST_SSH_KEY $SSH_OPTIONS" $WORKSPACE/logs $LOGS_HOST_USERNAME@$LOGS_HOST:$FULL_LOGS_PATH
 rm -rf $WORKSPACE/logs
