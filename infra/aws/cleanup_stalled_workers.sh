4964d4d7d2a4e51a3334facd297cd184d047f4a3
e-kuznetsov <evgeny.kuznetsov@outlook.com>
2019-12-17 20:32:29 +0300

    Rework cleanup aws script

--- infra/aws/cleanup_stalled_workers.sh
+++ infra/aws/cleanup_stalled_workers.sh
@@ -7,34 +7,32 @@ my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
 source "$my_dir/definitions"
-env|sort
-API_QUERY_RESULT=$(curl -k -s "${JENKINS_URL}computer/api/xml?tree=computer\[executors\[currentExecutable\[url\]\],oneOffExecutors\[currentExecutable\[url\]\]\]&xpath=//url&wrapper=builds")
-echo $API_QUERY_RESULT | grep  "${JOB_NAME}" || exit 1
-ACTIVE_PIPELINE_BUILDS=$(echo "$API_QUERY_RESULT" | sed  's/<[^>]*>/\n/g'  | sed '/^$/d' | sort | uniq | grep "${PIPELINE_NAME}" | awk -F "/" '{print $(NF-1)}')
+source "$WORKSPACE/global.env"
+
 aws ec2 describe-instances \
       --region "$AWS_REGION" \
-      --filters "Name=instance.group-id,Values=${AWS_SG}" \
-      --query 'Reservations[*].Instances[*].[InstanceId, Tags[?Key==`PipelineBuildTag`].Value | [0]]' \
-      --output text | \
-      grep "jenkins-${PIPELINE_NAME}" | grep -v -F "${ACTIVE_PIPELINE_BUILDS}" | awk '{print $1}'
-if [[ -n "$ACTIVE_PIPELINE_BUILDS" ]]; then
+      --filters "Name=instance.group-id,Values=${AWS_SG}" "Name=tag:SLAVE,Values=${SLAVE}" "Name=instance-state-code,Values=16" \
+      --query 'Reservations[*].Instances[*].[Tags[?Key==`PipelineBuildTag`].Value | [0]]' \
+      --output text | sort | uniq > existing_tags.txt
+
+curl -k -s "${JENKINS_URL}computer/api/xml?tree=computer\[executors\[currentExecutable\[url\]\],oneOffExecutors\[currentExecutable\[url\]\]\]&xpath=//url&wrapper=builds" | \
+    xpath -q -e '/builds/url' | sed -e "s#<url>${JENKINS_URL}job/#jenkins-#g" -e 's#/</url>##g' | tr '/' '-' | sort | uniq > running_jobs.txt
+
+if ! cat running_jobs.txt | grep "${JOB_NAME}-${BUILD_NUMBER}"; then
+ echo "Input validation failed"
+ exit 1
+fi
+
+TERMINATION_LIST_TAGS=$(comm -23 existing_tags.txt running_jobs.txt)
+
+if [[ -n "$TERMINATION_LIST_TAGS" ]]; then
   TERMINATION_LIST=$(aws ec2 describe-instances \
       --region "$AWS_REGION" \
       --filters "Name=instance.group-id,Values=${AWS_SG}" \
-      --query 'Reservations[*].Instances[*].[InstanceId, Tags[?Key==`PipelineBuildTag`].Value | [0]]' \
-      --output text | \
-      grep "jenkins-${PIPELINE_NAME}" | grep -v -F "${ACTIVE_PIPELINE_BUILDS}" | awk '{print $1}')
-  if [[ -n "$TERMINATION_LIST" ]]; then
-      aws ec2 terminate-instances --region "$AWS_REGION" --instance-ids "$TERMINATION_LIST"
-  fi
-else
-  TERMINATION_LIST=$(aws ec2 describe-instances \
-      --region $AWS_REGION \
-      --filters "Name=instance.group-id,Values=${AWS_SG}" \
-      --filters "Name=PipelineName,Values=${PIPELINE_NAME}" \
-      --query 'Instances[*]' \
-      --output text)
-  if [[ -n "$TERMINATION_LIST" ]]; then
-      aws ec2 terminate-instances --region "$AWS_REGION" --instance-ids "$TERMINATION_LIST"
-  fi
+                "Name=tag:SLAVE,Values=${SLAVE}" \
+                "Name=tag:PipelineBuildTag,Values=$(echo ${TERMINATION_LIST_TAGS} | tr ' ' ',')" \
+                "Name=instance-state-code,Values=16" \
+      --query 'Reservations[*].Instances[*].[InstanceId]' \
+      --output text )
+  aws ec2 terminate-instances --region "$AWS_REGION" --instance-ids "$TERMINATION_LIST"
 fi
