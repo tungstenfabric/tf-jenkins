572b41e0f1ef45dec6c70254dcd16da5bdbbfc68
e-kuznetsov <evgeny.kuznetsov@outlook.com>
2019-12-18 16:20:30 +0300

    Add cleanup job

--- jjb/pipelines/cleanup_stalled_workers.pipeline.groovy
+++ jjb/pipelines/cleanup_stalled_workers.pipeline.groovy
@@ -1,24 +1,65 @@
 pipeline{
     agent any
-    environment {
-        PIPELINE_NAME = "check-pipeline"
+    triggers{
+        cron('*/10 * * * *')
     }
     stages{
-        stage ('Cleanup AWS'){
+        stage('Checkout'){
+            steps {
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']],
+                    doGenerateSubmoduleConfigurations: false,
+                    extensions: [],
+                    submoduleCfg: [],
+                    extensions: [[$class: 'RelativeTargetDirectory', 
+                        relativeTargetDir: 'src/progmaticlab/tf-jenkins']],
+                    userRemoteConfigs: [[url: 'https://github.com/progmaticlab/tf-jenkins.git']]]
+                )
+                stash 'tf-jenkins'
+            }
+        }
+        stage('Cleanup stalled AWS Workers') {
             agent { label 'aws'}
             steps {
+                unstash 'tf-jenkins'
                 withCredentials(
                     bindings:
                         [[$class: 'AmazonWebServicesCredentialsBinding',
                             credentialsId: 'aws-creds',
                             accessKeyVariable: 'AWS_ACCESS_KEY_ID',
                             secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]){
-                sh """
-                    export DEBUG=true
-                    $WORKSPACE/src/progmaticlab/tf-jenkins/infra/aws/cleanup_stalled_workers.sh
-                """
+                    sh """
+                        export DEBUG=true
+                        export SLAVE="aws"
+                        $WORKSPACE/src/progmaticlab/tf-jenkins/infra/aws/cleanup_stalled_workers.sh
+                    """
                 }
             }
+        }
+        stage('Cleanup stalled VEXX Workers') {
+            agent { label 'vexxhost'}
+            steps {
+                cleanWs()
+                unstash 'tf-jenkins'
+                withCredentials(
+                    bindings:
+                        [string(credentialsId: 'VEXX_OS_USERNAME', variable: 'OS_USERNAME'),
+                        string(credentialsId: 'VEXX_OS_PROJECT_NAME', variable: 'OS_PROJECT_NAME'),
+                        string(credentialsId: 'VEXX_OS_PASSWORD', variable: 'OS_PASSWORD'),
+                        string(credentialsId: 'VEXX_OS_DOMAIN_NAME', variable: 'OS_USER_DOMAIN_NAME'),
+                        string(credentialsId: 'VEXX_OS_DOMAIN_NAME', variable: 'OS_PROJECT_DOMAIN_NAME'),
+                        string(credentialsId: 'VEXX_OS_AUTH_URL', variable: 'OS_AUTH_URL')]){
+                    sh """
+                        export SLAVE="vexxhost"
+                        export DEBUG=true
+                        $WORKSPACE/src/progmaticlab/tf-jenkins/infra/vexxhost/cleanup_stalled_workers.sh
+                    """
+                }
+            }
+        }
+    }
+    post {
+        cleanup {
+            deleteDir()
         }
     }
 }
