0cf5ceb7667c0b758b56606e568703737379ff3e
Ivan Timofeev <ivanv.timofeev@gmail.com>
2019-12-20 14:39:22 +0300

    Add deployer builder logs

--- jobs/build/run.sh
+++ jobs/build/run.sh
@@ -62,12 +62,23 @@ export PATH=\$PATH:/usr/sbin
 
 sudo docker cp tf-developer-sandbox:/root/contrail/contrail-container-builder/containers .
 sudo find ./containers/ -not -name '*.log' -delete &>/dev/null || /bin/true
-tar -czf containers.tgz containers
-sudo rm -rf ./containers
+sudo mv containers container-builder
+tar -czf container-builder.tgz container-builder
+sudo rm -rf ./container-builder
+
+sudo docker cp tf-developer-sandbox:/root/contrail/contrail-deployers-containers/containers .
+sudo find ./containers/ -not -name '*.log' -delete &>/dev/null || /bin/true
+sudo mv containers deployer-builder
+tar -czf deployer-builder.tgz deployer-builder
+sudo rm -rf ./deployer-builder
 EOF
 
-rsync -a -e "ssh -i ${WORKER_SSH_KEY} ${SSH_OPTIONS}" ${IMAGE_SSH_USER}@${instance_ip}:containers.tgz ${WORKSPACE}/
-tar -zxvf  ${WORKSPACE}/containers.tgz
+rsync -a -e "ssh -i ${WORKER_SSH_KEY} ${SSH_OPTIONS}" ${IMAGE_SSH_USER}@${instance_ip}:container-builder.tgz ${WORKSPACE}/
+tar -zxvf  ${WORKSPACE}/container-builder.tgz
+
+rsync -a -e "ssh -i ${WORKER_SSH_KEY} ${SSH_OPTIONS}" ${IMAGE_SSH_USER}@${instance_ip}:deployer-builder.tgz ${WORKSPACE}/
+tar -zxvf  ${WORKSPACE}/deployer-builder.tgz
 
 ssh -i ${LOGS_HOST_SSH_KEY} ${SSH_OPTIONS} ${LOGS_HOST_USERNAME}@${LOGS_HOST} "mkdir -p ${FULL_LOGS_PATH}"
-rsync -a -e "ssh -i ${LOGS_HOST_SSH_KEY} ${SSH_OPTIONS}" ${WORKSPACE}/containers ${LOGS_HOST_USERNAME}@${LOGS_HOST}:${FULL_LOGS_PATH} || /bin/true
+rsync -a -e "ssh -i ${LOGS_HOST_SSH_KEY} ${SSH_OPTIONS}" ${WORKSPACE}/container-builder ${LOGS_HOST_USERNAME}@${LOGS_HOST}:${FULL_LOGS_PATH} || /bin/true
+rsync -a -e "ssh -i ${LOGS_HOST_SSH_KEY} ${SSH_OPTIONS}" ${WORKSPACE}/deployer-builder ${LOGS_HOST_USERNAME}@${LOGS_HOST}:${FULL_LOGS_PATH} || /bin/true
