6feab371db30e46496248e9ff00f961b2232c56f
Andrey Pavlov <andrey-mp@yandex.ru>
2019-12-22 23:21:32 +0300

    print more debug

--- jjb/pipelines/check.groovy
+++ jjb/pipelines/check.groovy
@@ -283,33 +283,29 @@ def evaluate_env() {
 
 def get_jobs(project) {
   jobs = [:]
-  try {
-    def data = readYaml file: "${WORKSPACE}/src/progmaticlab/tf-jenkins/config/projects.yaml"
-    def templates = [:]
-    for (item in data) {
-      if (item.containsKey('project-template')) {
-        template = item.get('project-template')
-        templates[template.name] = template
-      }
+  def data = readYaml file: "${WORKSPACE}/src/progmaticlab/tf-jenkins/config/projects.yaml"
+  println data
+  def templates = [:]
+  for (item in data) {
+    if (item.containsKey('project-template')) {
+      template = item.get('project-template')
+      templates[template.name] = template
     }
-    for (item in data) {
-      if (!item.containsKey('project') || item.get('project').name != project)
-        continue
-      project = item.get('project')
-      if (project.containsKey('templates')) {
-        for (template in project.templates) {
-          for (job_item in templates[template].check.jobs) {
-            add_job(jobs, job_item)
-          }
+  }
+  for (item in data) {
+    if (!item.containsKey('project') || item.get('project').name != project)
+      continue
+    project = item.get('project')
+    if (project.containsKey('templates')) {
+      for (template in project.templates) {
+        for (job_item in templates[template].check.jobs) {
+          add_job(jobs, job_item)
         }
       }
-      for (job_item in project.check.jobs) {
-        add_job(jobs, job_item)
-      }
     }
-  } catch(err) {
-    println "Error in get_jobs: ${err.getMessage()}"
-    throw(err)
+    for (job_item in project.check.jobs) {
+      add_job(jobs, job_item)
+    }
   }
   return jobs
 }
