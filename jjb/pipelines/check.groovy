77b38e3c7e72bcd351c89b5c2aec41b99566659d
Andrey Pavlov <andrey-mp@yandex.ru>
2019-12-22 23:34:02 +0300

    run stash on master node

--- jjb/pipelines/check.groovy
+++ jjb/pipelines/check.groovy
@@ -16,8 +16,10 @@ inner_jobs_code = [:]
 timestamps {
   try {
     timeout(time: 4, unit: 'HOURS') {
-      sh "pwd ; hostname; ls -la"
-      stash(includes: 'src/progmaticlab/tf-jenkins/config/projects.yaml', name: 'projects')
+      node('master') {
+        sh "pwd ; hostname; ls -la"
+        stash(includes: 'src/progmaticlab/tf-jenkins/config/projects.yaml', name: 'projects')
+      }
       node("${SLAVE}") {
         stage('Pre-build') {
           unstash(name: 'projects')
