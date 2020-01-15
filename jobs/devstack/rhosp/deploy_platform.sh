224c65a1e5b6bee2ea532c91bb5e80532795ef82
Gleb Galkin <gleb108@gmail.com>
2020-01-15 16:17:19 +0300

    bugfix for rhosp job

--- jobs/devstack/rhosp/deploy_platform.sh
+++ jobs/devstack/rhosp/deploy_platform.sh
@@ -13,10 +13,10 @@ source $ENV_FILE
 
 echo "INFO: Deploy platform for $JOB_NAME"
 
-rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS" $WORKSPACE/src $IMAGE_SSH_USER@$mgmt_ip:./
+rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS" $WORKSPACE/src/tungstenfabric/tf-devstack $IMAGE_SSH_USER@$mgmt_ip:./
 
 #Copy ssh key to undercloud
-scp -i $WORKER_SSH_KEY $SSH_OPTIONS $WORKER_SSH_KEY $IMAGE_SSH_USER@$mgmt_ip:./ssh/id_rsa
+rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS" $WORKER_SSH_KEY $IMAGE_SSH_USER@$mgmt_ip:./ssh/id_rsa
 ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $IMAGE_SSH_USER@$mgmt_ip 'ssh-keygen -y -f ./ssh/id_rsa >./ssh/id_rsa.pub'
 ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $IMAGE_SSH_USER@$mgmt_ip chmod 600 ./ssh/id_rsa*
 
@@ -29,7 +29,7 @@ export RHEL_PASSWORD=$RHEL_PASSWORD
 #export CONTAINER_REGISTRY="$CONTAINER_REGISTRY"
 #export CONTRAIL_CONTAINER_TAG="$CONTRAIL_CONTAINER_TAG"
 export PATH=\$PATH:/usr/sbin
-cd src/tungstenfabric/tf-devstack/rhosp
+cd tf-devstack/rhosp
 ./run.sh platform
 EOF
 
