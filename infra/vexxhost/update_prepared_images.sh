bc9edd819e8173b702ecf15c6d7533f1a4d1bb2c
e-kuznetsov <evgeny.kuznetsov@outlook.com>
2020-01-15 20:02:41 +0300

    Fix typo

--- infra/vexxhost/update_prepared_images.sh
+++ infra/vexxhost/update_prepared_images.sh
@@ -8,7 +8,7 @@ my_dir="$(dirname $my_file)"
 source "$my_dir/definitions"
 
 for i in "${!OS_IMAGE_USERS[@]}"; do
-  packer build -machine-readable -var "os_image=${i,,}" -var "ssh_user=${OS_IMAGE_USERS[$i]}" -var "net_id=$OS_NETWORK_ID" \
+  packer build -machine-readable -var "os_image=${i,,}" -var "ssh_user=${OS_IMAGE_USERS[$i]}" \
       ${WORKSPACE}/src/progmaticlab/tf-jenkins/infra/packer/vexxhost.json
   OLD_IMAGES=$(openstack image list --tag ${i,,} -c Name -f value | sort -nr | tail -n +4)
   for o in $OLD_IMAGES; do
