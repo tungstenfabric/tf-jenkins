3ca6a1c79ad80f71d4cd953ef899d719e4d5a26d
e-kuznetsov <evgeny.kuznetsov@outlook.com>
2020-01-16 13:37:59 +0300

    Enable rhel server repos

--- infra/packer/rhel7.sh
+++ infra/packer/rhel7.sh
@@ -2,5 +2,6 @@
 set -o pipefail
 
 sudo subscription-manager register --username "$RHEL_USER" --password "$RHEL_PASS"
+sudo subscription-manager repos --enable rhel-7-server-rpms rhel-7-server-extras-rpms rhel-7-server-optional-rpms
 sudo yum upgrade -y
 sudo subscription-manager unregister --username="$RHEL_USER"
