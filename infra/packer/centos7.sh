db578c7fe9814e5c8f86f1cbf5cf9fd2d3be8fcb
e-kuznetsov <evgeny.kuznetsov@outlook.com>
2020-01-16 14:12:16 +0300

    Add subscription pool_id

--- infra/packer/centos7.sh
+++ infra/packer/centos7.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -eE
 set -o pipefail
 
-sudo yum upgrade -y
+sudo yum update -y
--- infra/packer/rhel7.sh
+++ infra/packer/rhel7.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -eE
 set -o pipefail
 
-sudo subscription-manager register --username "$RHEL_USER" --password "$RHEL_PASS"
-sudo subscription-manager repos --enable rhel-7-server-rpms rhel-7-server-extras-rpms rhel-7-server-optional-rpms
-sudo yum upgrade -y
-sudo subscription-manager unregister --username="$RHEL_USER"
+sudo subscription-manager register --username "$RHEL_USER" --password "$RHEL_PASSWORD"
+sudo subscription-manager attach --pool $RHEL_POOL_ID
+sudo subscription-manager repos --enable=rhel-7-server-rpms \
+                                --enable=rhel-7-server-extras-rpms \
+                                --enable=rhel-7-server-optional-rpms
+sudo yum update -y
+sudo subscription-manager unregister
