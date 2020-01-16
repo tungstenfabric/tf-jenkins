5b23ebcd2da43b2da32c5c8fd7600ac4fd4f9b2c
e-kuznetsov <evgeny.kuznetsov@outlook.com>
2020-01-16 19:19:30 +0300

    Fix rhel8 repo list

--- infra/packer/rhel8.sh
+++ infra/packer/rhel8.sh
@@ -3,8 +3,7 @@ set -o pipefail
 
 sudo subscription-manager register --username "$RHEL_USER" --password "$RHEL_PASSWORD"
 sudo subscription-manager attach --pool $RHEL_POOL_ID
-sudo subscription-manager repos --enable=rhel-8-server-rpms \
-                                --enable=rhel-8-server-extras-rpms \
-                                --enable=rhel-8-server-optional-rpms
+sudo subscription-manager repos --enable=rhel-8-for-x86_64-baseos-rpms \
+                                --enable=rhel-8-for-x86_64-appstream-rpms
 sudo yum update -y
 sudo subscription-manager unregister
