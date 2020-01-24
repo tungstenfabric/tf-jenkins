c96ba7614fce0d47540bf34486cedfeacaa5e247
e-kuznetsov <evgeny.kuznetsov@outlook.com>
2020-01-24 20:36:02 +0300

    Add vcpu max count

--- infra/vexxhost/create_workers.sh
+++ infra/vexxhost/create_workers.sh
@@ -33,7 +33,12 @@ fi
 
 # wait for free resource
 while true; do
-  [[ "$(($(nova list --tags "SLAVE=$SLAVE"  --field status | grep -c 'ID\|ACTIVE') - 1))" -lt "$MAX_COUNT" ]] && break
+  [[ "$(($(nova list --tags "SLAVE=$SLAVE"  --field status | grep -c 'ID\|ACTIVE') - 1))" -lt "$MAX_COUNT_VM" ]] && break
+  sleep 60
+done
+#ToDo: Use the number of flavor vcpu
+while true; do
+  [[ "$(nova quota-show --detail | grep cores | sed 's/}.*/}/'| tr -d "}" | awk '{print $NF}')" -lt "$MAX_COUNT_VCPU" ]] && break
   sleep 60
 done
 
