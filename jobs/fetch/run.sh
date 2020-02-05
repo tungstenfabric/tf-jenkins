2828b894afcb587590dffe15f13f98c21fddb027
alexey-mr <alexey.morlang@gmail.com>
2020-02-05 23:09:38 +0300

    Fix rhel

--- jobs/fetch/run.sh
+++ jobs/fetch/run.sh
@@ -7,5 +7,9 @@ my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
 source "$my_dir/definitions"
+# do it as a latest source to override all exports
+if [[ -f ${WORKSPACE}/${JOB_NAME}.env ]]; then
+  source ${WORKSPACE}/${JOB_NAME}.env
+fi
 
 ${my_dir}/run_${BUILD_WORKER["${ENVIRONMENT_OS^^}"]}.sh
