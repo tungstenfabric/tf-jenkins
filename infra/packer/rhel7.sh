a1d108c6504e004a0ef52ad23e58ecdf9c8b3ba4
e-kuznetsov <evgeny.kuznetsov@outlook.com>
2020-02-11 14:11:30 +0300

    Switch rhel7 to local repo

--- infra/packer/rhel7.sh
+++ infra/packer/rhel7.sh
@@ -1,10 +1,37 @@
 #!/bin/bash -eE
 set -o pipefail
 
-sudo subscription-manager register --username "$RHEL_USER" --password "$RHEL_PASSWORD"
-sudo subscription-manager attach --pool $RHEL_POOL_ID
-sudo subscription-manager repos --enable=rhel-7-server-rpms \
-                                --enable=rhel-7-server-extras-rpms \
-                                --enable=rhel-7-server-optional-rpms
+
+cat << EOF > local.repo
+[local-rhel-7-server-rpms]
+name = Red Hat Enterprise Linux 7 Server (RPMs) local
+baseurl = http://rhel-mirrors.tf-jenkins.progmaticlab.com/rhel-7-server-rpms
+enabled = 1
+gpgcheck = 1
+gpgkey = file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
+
+[local-rhel-7-server-optional-rpms]
+name = Red Hat Enterprise Linux 7 Server - Optional (RPMs) local
+baseurl = http://rhel-mirrors.tf-jenkins.progmaticlab.com/rhel-7-server-optional-rpms
+enabled = 1
+gpgcheck = 1
+gpgkey = file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
+
+[local-rhel-7-server-extras-rpms]
+name = Red Hat Enterprise Linux 7 Server - Extras (RPMs) local
+baseurl = http://rhel-mirrors.tf-jenkins.progmaticlab.com/rhel-7-server-extras-rpms
+enabled = 1
+gpgcheck = 1
+gpgkey = file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
+
+[local-rhel-7-server-openstack-13-rpms]
+name = Red Hat OpenStack Platform 13 for RHEL 7 (RPMs) local
+baseurl = http://rhel-mirrors.tf-jenkins.progmaticlab.com/rhel-7-server-openstack-13-rpms
+enabled = 1
+gpgcheck = 1
+gpgkey = file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
+EOF
+
+sudo mv local.repo /etc/yum.repos.d/
+
 sudo yum update -y
-sudo subscription-manager unregister
