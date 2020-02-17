5deec55ecfdc49de428420e57520594bc9741fef
e-kuznetsov <evgeny.kuznetsov@outlook.com>
2020-02-17 14:43:10 +0300

    Fix packer scripts

--- infra/packer/ubuntu16.sh
+++ infra/packer/ubuntu16.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -eE
 set -o pipefail
 
-sudo apt-get update -y
-sudo apt remove popularity-contest
-sudo dpkg-reconfigure -plow unattended-upgrades
 systemctl disable --now apt-daily{,-upgrade}.{timer,service}
+sudo apt-get update -y
 sudo apt-get upgrade -y
--- infra/packer/ubuntu18.sh
+++ infra/packer/ubuntu18.sh
@@ -2,7 +2,5 @@
 set -o pipefail
 
 sudo apt-get update -y
-sudo apt remove popularity-contest
-sudo dpkg-reconfigure -plow unattended-upgrades
 systemctl disable --now apt-daily{,-upgrade}.{timer,service}
 sudo apt-get upgrade -y
