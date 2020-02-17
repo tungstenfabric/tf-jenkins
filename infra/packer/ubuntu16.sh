0bf7f37b6c99a802e0bb424d7621159d88737f3e
e-kuznetsov <evgeny.kuznetsov@outlook.com>
2020-02-17 16:51:05 +0300

    Fix packer scripts

--- infra/packer/ubuntu16.sh
+++ infra/packer/ubuntu16.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eE
 set -o pipefail
 
-systemctl disable --now apt-daily{,-upgrade}.{timer,service}
+sudo systemctl disable --now apt-daily{,-upgrade}.{timer,service}
 sudo apt-get update -y
 sudo apt-get upgrade -y
--- infra/packer/ubuntu18.sh
+++ infra/packer/ubuntu18.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eE
 set -o pipefail
 
+sudo systemctl disable --now apt-daily{,-upgrade}.{timer,service}
 sudo apt-get update -y
-systemctl disable --now apt-daily{,-upgrade}.{timer,service}
 sudo apt-get upgrade -y
