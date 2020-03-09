f219dda5059bfab052254109fa78b3eef26a9e2b
Andrey Pavlov <andrey-mp@yandex.ru>
2020-03-09 09:29:08 +0300

    remove ansible hack from deploy-platform

--- jobs/devstack/ansible/deploy_platform.sh
+++ jobs/devstack/ansible/deploy_platform.sh
@@ -10,13 +10,6 @@ source "$my_dir/definitions"
 
 echo "INFO: Deploy platform for $JOB_NAME"
 
-# target 'platform' tries to fetch_deployer and use code from that container.
-# but for gerrit's check this container will be created in hour in build job.
-# as a workaround we will skip deploy platform here.
-# next solution is to use tf-developer-sandbox image which was created
-# by fetch job (but with different DEPLOYER_DIR)
-exit 0
-
 rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS" $WORKSPACE/src $IMAGE_SSH_USER@$instance_ip:./
 
 cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $IMAGE_SSH_USER@$instance_ip || res=1
