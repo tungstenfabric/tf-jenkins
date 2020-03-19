d271e3db00f3f891d8ce8186a9f44f901a8e9ae5
Ivan Timofeev <ivanv.timofeev@gmail.com>
2020-03-19 16:32:07 +0300

    Add alter main.groovy for debug concurrent gating

---
+++ pipelines/main.cg.groovy
@@ -0,0 +1,328 @@
+// This pipeline is temporary used for debugging
+// concurent gaing
+// if you have a question write here: itimofeev@progmaticlab.com
+import groovy.json.JsonOutput;
+
+// constansts
+TIMEOUT_HOURS = 4
+REGISTRY_IP = "pnexus.sytes.net"
+REGISTRY_PORT = "5001"
+LOGS_HOST = "pnexus.sytes.net"
+LOGS_BASE_PATH = "/var/www/logs/jenkins_logs"
+LOGS_BASE_URL = "http://pnexus.sytes.net:8082/jenkins_logs"
+if (env.GERRIT_PIPELINE == 'nightly') {
+  TIMEOUT_HOURS = 6
+  REGISTRY_PORT = "5002"
+}
+
+// pipeline flow variables
+// base url for all jobs
+logs_url = ""
+// set of result for each job 
+job_results = [:]
+
+rnd = new Random()
+
+// gerrit utils
+gerrit_utils = null
+// config utils
+config_utils = null
+// jobs utils
+jobs_utils = null
+
+timestamps {
+  timeout(time: TIMEOUT_HOURS, unit: 'HOURS') {
+    node("${SLAVE}") {
+      if (!env.GERRIT_CHANGE_ID && env.GERRIT_PIPELINE != 'nightly') {
+        println("Manual run is forbidden")
+        return
+      }
+
+      stage('init') {
+        cleanWs(disableDeferredWipeout: true, notFailBuild: true, deleteDirs: true)
+        clone_self()
+        gerrit_utils = load("${WORKSPACE}/tf-jenkins/pipelines/utils/gerrit.groovy")
+        config_utils = load("${WORKSPACE}/tf-jenkins/pipelines/utils/config.groovy")
+        jobs_utils = load("${WORKSPACE}/tf-jenkins/pipelines/utils/jobs.groovy")
+      }
+      if (env.GERRIT_PIPELINE == 'gate' && !gerrit_utils.has_gate_approvals()) {
+            println("There is no gate approvals.. skip gate")
+            return
+      }
+
+      def streams = [:]
+      def jobs = [:]
+      def post_jobs = [:]
+      pre_build_done = false
+      try {
+        time_start = (new Date()).getTime()
+        stage('Pre-build') {
+          terminate_previous_runs()
+          (streams, jobs, post_jobs) = evaluate_env()
+          gerrit_utils.gerrit_build_started()
+
+          desc = "<a href='${logs_url}'>${logs_url}</a>"
+          if (env.GERRIT_CHANGE_ID) {
+            desc += "<br>Project: ${env.GERRIT_PROJECT}"
+            desc += "<br>Branch: ${env.GERRIT_BRANCH}"
+          }
+          currentBuild.description = desc
+          pre_build_done = true
+        }
+
+        jobs_utils.run_jobs(jobs)
+      } finally {
+        println(job_results)
+        stage('gerrit vote') {
+          // add gerrit voting +2 +1 / -1 -2
+          verified = gerrit_utils.gerrit_vote(pre_build_done, streams, jobs, job_results, (new Date()).getTime() - time_start)
+          sh """#!/bin/bash -e
+          echo "export VERIFIED=${verified}" >> global.env
+          """
+          archiveArtifacts(artifacts: 'global.env')
+        }
+        jobs_utils.run_jobs(post_jobs)
+
+        save_pipeline_output_to_logs()
+      }
+    }
+  }
+}
+
+
+def clone_self() {
+  checkout([
+    $class: 'GitSCM',
+    branches: [[name: "*/master"]],
+    doGenerateSubmoduleConfigurations: false,
+    submoduleCfg: [],
+    userRemoteConfigs: [[url: 'https://github.com/progmaticlab/tf-jenkins.git']],
+    extensions: [
+      [$class: 'CleanBeforeCheckout'],
+      [$class: 'CloneOption', depth: 1],
+      [$class: 'RelativeTargetDirectory', relativeTargetDir: 'tf-jenkins']
+    ]
+  ])
+}
+
+def evaluate_env() {
+  try {
+    sh """#!/bin/bash -e
+      echo "export PIPELINE_BUILD_TAG=${BUILD_TAG}" > global.env
+      echo "export SLAVE=${SLAVE}" >> global.env
+    """
+
+    // evaluate logs params
+    if (env.GERRIT_CHANGE_ID) {
+      contrail_container_tag = env.GERRIT_CHANGE_NUMBER + '-' + env.GERRIT_PATCHSET_NUMBER
+      hash = env.GERRIT_CHANGE_NUMBER.reverse().take(2).reverse()
+      logs_path = "${LOGS_BASE_PATH}/gerrit/${hash}/${env.GERRIT_CHANGE_NUMBER}/${env.GERRIT_PATCHSET_NUMBER}/${env.GERRIT_PIPELINE}_${BUILD_NUMBER}"
+      logs_url = "${LOGS_BASE_URL}/gerrit/${hash}/${env.GERRIT_CHANGE_NUMBER}/${env.GERRIT_PATCHSET_NUMBER}/${env.GERRIT_PIPELINE}_${BUILD_NUMBER}"
+    } else if (env.GERRIT_PIPELINE == 'nightly') {
+      contrail_container_tag = 'nightly'
+      logs_path = "${LOGS_BASE_PATH}/nightly/pipeline_${BUILD_NUMBER}"
+      logs_url = "${LOGS_BASE_URL}/nightly/pipeline_${BUILD_NUMBER}"
+    } else {
+      contrail_container_tag = 'dev'
+      logs_path = "${LOGS_BASE_PATH}/manual/pipeline_${BUILD_NUMBER}"
+      logs_url = "${LOGS_BASE_URL}/manual/pipeline_${BUILD_NUMBER}"
+    }
+    println("Logs URL: ${logs_url}")
+    sh """#!/bin/bash -e
+      echo "export LOGS_HOST=${LOGS_HOST}" >> global.env
+      echo "export LOGS_PATH=${logs_path}" >> global.env
+      echo "export LOGS_URL=${logs_url}" >> global.env
+    """
+    // store default registry params. jobs can redefine them if needed in own config (VARS).
+    sh """#!/bin/bash -e
+      echo "export REGISTRY_IP=${REGISTRY_IP}" >> global.env
+      echo "export REGISTRY_PORT=${REGISTRY_PORT}" >> global.env
+      echo "export CONTAINER_REGISTRY=${REGISTRY_IP}:${REGISTRY_PORT}" >> global.env
+      echo "export CONTRAIL_CONTAINER_TAG=${contrail_container_tag}" >> global.env
+    """
+
+    // store gerrit input if present. evaluate jobs
+    println("Pipeline to run: ${env.GERRIT_PIPELINE}")
+    project_name = env.GERRIT_PROJECT
+    if (env.GERRIT_CHANGE_ID) {
+      url = gerrit_utils.resolve_gerrit_url()
+      sh """#!/bin/bash -e
+        echo "export GERRIT_URL=${url}" >> global.env
+        echo "export GERRIT_CHANGE_ID=${env.GERRIT_CHANGE_ID}" >> global.env
+        echo "export GERRIT_BRANCH=${env.GERRIT_BRANCH}" >> global.env
+      """
+    } else if (env.GERRIT_PIPELINE == 'nightly') {
+      project_name = "tungstenfabric"
+    }
+    archiveArtifacts(artifacts: 'global.env')
+
+    (streams, jobs, post_jobs) = config_utils.get_jobs(project_name, env.GERRIT_PIPELINE)
+    println("Streams from  config: ${streams}")
+    println("Jobs from config: ${jobs}")
+    println("Post Jobs from config: ${post_jobs}")
+  } catch (err) {
+    msg = err.getMessage()
+    if (err != null) {
+      println("ERROR: Failed set environment ${msg}")
+    }
+    throw(err)
+  }
+  return [streams, jobs, post_jobs]
+}
+
+def terminate_previous_runs() {
+  if (!env.GERRIT_CHANGE_ID)
+    return
+
+  def runningBuilds = Jenkins.getInstanceOrNull().getView('All').getBuilds().findAll() { it.getResult().equals(null) }
+  for (rb in runningBuilds) {
+    def action = rb.allActions.find { it in hudson.model.ParametersAction }
+    if (!action)
+      continue
+    gerrit_change_number = action.getParameter("GERRIT_CHANGE_NUMBER")
+    if (!gerrit_change_number) {
+      continue
+    }
+    change_num = gerrit_change_number.value.toInteger()
+    patchset_num = action.getParameter("GERRIT_PATCHSET_NUMBER").value.toInteger()
+    if (GERRIT_CHANGE_NUMBER.toInteger() == change_num && GERRIT_PATCHSET_NUMBER.toInteger() > patchset_num) {
+      rb.doStop()
+      println "Build $rb has been aborted when a new patchset is created"
+    }
+  }
+}
+
+def save_pipeline_output_to_logs() {
+  println("BUILD_URL = ${BUILD_URL}consoleText")
+  withCredentials(
+    bindings: [
+      sshUserPrivateKey(credentialsId: 'logs_host', keyFileVariable: 'LOGS_HOST_SSH_KEY', usernameVariable: 'LOGS_HOST_USERNAME')]) {
+    sh """#!/bin/bash -e
+      set -x
+      curl ${BUILD_URL}consoleText > pipelinelog.txt 
+      ssh -i ${LOGS_HOST_SSH_KEY} -T -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ${LOGS_HOST_USERNAME}@${LOGS_HOST} "mkdir -p ${logs_path}"
+      rsync -a -e "ssh -i ${LOGS_HOST_SSH_KEY} -T -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" pipelinelog.txt ${LOGS_HOST_USERNAME}@${LOGS_HOST}:${logs_path} 
+    """
+  }
+  archiveArtifacts artifacts: "pipelinelog.txt"
+  echo "Output logs saved at ${logs_url}/pipelinelog.txt"
+}
+
+// Prepare map of gate pipeline builds for choose
+// base gating image if concurent mode enabled
+def create_gate_builds_map(){
+  def builds_map = [:]
+  // Get through all gate's builds
+  def job = jenkins.model.Jenkins.instance.getItem('pipeline-gate-opencontrail')
+  println("DEBUG: job.builds class is ${job.builds.class}")
+  job.builds.each {
+    def build = it
+    def build_id = build.getEnvVars().BUILD_ID
+    def build_status = build.getResult().toString()
+    // by defailt we apply failed status to build. It can be changed later
+    // after we get VERIFIED flag from global.env
+    builds_map[build_id] = [status:build_status]
+
+    def artifactManager =  build.getArtifactManager()
+
+    if (artifactManager.root().isDirectory()) {
+      def fileList = artifactManager.root().list()
+      fileList.each {
+        def file = it
+        if(file.toString().contains('global.env')) {
+          // extract global.env artifact for each build if exists
+          def fileText = it.open().getText()
+          fileText.split("\n").each {
+            def line = it
+            // Check if CONTRAIL_CONTAINER_TAG or DEVENVTAG exists in global.env file
+            // store theil values into builds_map
+            if(line.contains('CONTRAIL_CONTAINER_TAG')) {
+              def container_tag = line.split('=')[1].trim()
+              builds_map[build_id]['container_tag'] = container_tag
+            }
+            if(line.contains('DEVENVTAG')) {
+              def devenv_tag = line.split('=')[1].trim()
+              builds_map[build_id]['devenv_tag'] = devenv_tag
+            }
+            if(line.contains('VERIFIED')) {
+              def verified = line.split('=')[1].trim()
+              builds_map[build_id]['verified'] = verified
+            }
+          }
+        }
+      }
+    }
+    // Check build status based on VERIFIED value from global.env
+    if( build_status == 'FAILED' &&
+        builds_map[build_id].containsKey('verified') &&
+        builds_map[build_id]['verified'].toInteger() > 0){
+      builds_map[build_id]['status'] = 'SUCCESS'
+    }
+    if( build_status == 'SUCCESS' &&
+        (! builds_map[build_id].containsKey('verified') || builds_map[build_id]['verified'].toInteger() <= 0 )){
+      builds_map[build_id]['status'] = 'FAILURE'
+    }
+  }
+  return builds_map
+}
+
+def set_devenv_tag(builds_map){
+  builds_map.any {
+    def build = it.value
+    def build_no = it.key
+    println("DEBUG: build_no = ${build_no} build = ${build}")
+    if(build_no == BUILD_ID){
+      // skip current build
+      return false
+    }
+    if(build['status'] == "FAILURE")
+      // continue iterate to SUCCESS build or build in progress
+      return false
+    else if( build['status'] == "SUCCESS")
+      // We meet Sucess build - no any DEVENVTAG needed - break loop
+      return true
+    else
+      // We meet inprogress build but perhaps it is already fails
+      // it is not failed if build not have devenv_tag
+      // or if is_build_fail - recursive function return true
+      if(!build.containsKey('devenv_tag') || is_build_fail(build['devenv_tag'])){
+        // build is in the process and it is not failed - we can use its image
+        // for start next build
+        sh """#!/bin/bash -e
+          echo "export DEVENVTAG=${build['container_tag']}" >> global.env
+        """
+        archiveArtifacts(artifacts: 'global.env')
+        return true
+      }
+  }
+}
+
+def is_build_fail(devenv_tag, builds_map) {
+  def is_build_not_fail = false
+  builds_map.any {
+    def build = it.value
+    if(build['container_tag'] != devenv_tag)
+      // iterate up to build while we not meet container_tag equal devenv_tag
+      return false
+    else if(build['status'] == 'SUCCESS'){
+      // Build is not fail
+      is_build_not_fail = true
+      return true
+    }
+    else if(build['status'] == 'FAILURE'){
+      // Build has been failed
+      return true
+    // Here we know - build is not finished yet
+    } else if(! build.containsKey('devenv_tag')){
+      // Buils is in process and it not based on any DEVENVTAG image
+      // therefore it not fails
+      is_build_not_fail = true
+      return true
+    } else{
+      // We need one recursive call more to check if this build is not fail
+      is_build_not_fail = is_build_fail(build['devenv_tag'],builds_map)
+      return true
+    }
+  }
+  return is_build_not_fail
+}
