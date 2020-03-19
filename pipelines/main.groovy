4a21f85ba0fb3d0174f6fc8a832f2072421fc0bb
Ivan Timofeev <ivanv.timofeev@gmail.com>
2020-03-19 15:57:20 +0300

    Restore gate pipeline to noconcurrent mode

--- pipelines/main.groovy
+++ pipelines/main.groovy
@@ -42,11 +42,10 @@ timestamps {
         config_utils = load("${WORKSPACE}/tf-jenkins/pipelines/utils/config.groovy")
         jobs_utils = load("${WORKSPACE}/tf-jenkins/pipelines/utils/jobs.groovy")
       }
-      //TODO Uncomment after concurrent mode will be ready
-      /* if (env.GERRIT_PIPELINE == 'gate' && !gerrit_utils.has_gate_approvals()) {
+      if (env.GERRIT_PIPELINE == 'gate' && !gerrit_utils.has_gate_approvals()) {
             println("There is no gate approvals.. skip gate")
             return
-        } */
+      }
 
       def streams = [:]
       def jobs = [:]
@@ -66,20 +65,6 @@ timestamps {
           }
           currentBuild.description = desc
           pre_build_done = true
-        }
-
-        if (env.GERRIT_PIPELINE == 'gate'){
-          // Choose base image for gating pipeline if
-          // some gating builds are in process
-          builds_map = create_gate_builds_map()
-          println("INFO: prepare builds_map = ${builds_map} ")
-          set_devenv_tag(builds_map)
-          println("INFO: jobs are: ${jobs}")
-
-// Run fetch_sources - remove after debugging
-          jobs_utils.run_jobs(["fetch-sources-centos":["job-name":"fetch-sources"]])
-//TODO Gating Pipeline ends Here now. Remove when gating will be done
-          return
         }
 
         jobs_utils.run_jobs(jobs)
