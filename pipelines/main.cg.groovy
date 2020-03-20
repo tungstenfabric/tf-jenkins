295df10a953838b3e148f5012ea32b7b6001c99d
Ivan Timofeev <ivanv.timofeev@gmail.com>
2020-03-20 12:09:16 +0300

    Info for more verbosity

--- pipelines/main.cg.groovy
+++ pipelines/main.cg.groovy
@@ -375,6 +375,7 @@ def gate_wait_for_fetch(build_no){
   // Find fetch-sounces job for our build
   def fetch_job = null
   while((fetch_job = gate_lookup_fetch_job(fetch_jobs, build_no) != null)){
+    println("INFO: Waiting for fetch_job will be started")
     sleep(5)
     // check if build is not fail at last 5 sec
     // if build is not finished yet,
@@ -388,6 +389,7 @@ def gate_wait_for_fetch(build_no){
 
   // Wait for fetch job finished
   while( ! fetch_job.getResult().toString()){
+    println("INFO: Waiting for fetch job will finished")
     sleep(5)
   }
 
