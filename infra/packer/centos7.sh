1ef04ed259d6e5983eb352ebc99103e7b23b441d
Comrade88 <shrike_@mail.ru>
2020-03-20 10:39:11 +0000

    use nexus yum repos in centos7 build

--- infra/packer/centos7.sh
+++ infra/packer/centos7.sh
@@ -1,4 +1,15 @@
 #!/bin/bash -eE
 set -o pipefail
 
+yum-config-manager --disable \*
+yum-config-manager --add-repo http://pnexus.sytes.net/repository/centos7-centosplus
+sed -i '$ i gpgcheck=0' /etc/yum.repos.d/pnexus.sytes.net_repository_centos7-centosplus.repo
+yum-config-manager --add-repo http://pnexus.sytes.net/repository/centos7-extras
+sed -i '$ i gpgcheck=0' /etc/yum.repos.d/pnexus.sytes.net_repository_centos7-extras.repo
+yum-config-manager --add-repo http://pnexus.sytes.net/repository/centos7-os
+sed -i '$ i gpgcheck=0' /etc/yum.repos.d/pnexus.sytes.net_repository_centos7-os.repo
+yum-config-manager --add-repo http://pnexus.sytes.net/repository/centos7-updates
+sed -i '$ i gpgcheck=0' /etc/yum.repos.d/pnexus.sytes.net_repository_centos7-updates.repo
+yum-config-manager --add-repo http://pnexus.sytes.net/repository/epel
+sed -i '$ i gpgcheck=0' /etc/yum.repos.d/pnexus.sytes.net_repository_epel.repo
 sudo yum update -y
