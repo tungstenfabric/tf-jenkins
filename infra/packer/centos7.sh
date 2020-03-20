cdfa7c9f787fb5f0aaae77d585173640f4bc45d4
Comrade88 <shrike_@mail.ru>
2020-03-20 13:44:47 +0000

    wrap the script with sudo
    all the commands require privileges

--- infra/packer/centos7.sh
+++ infra/packer/centos7.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -eE
 set -o pipefail
 
+sudo -- sh <<MANY
 yum-config-manager --disable \*
-yum-config-manager --add-repo http://pnexus.sytes.net/repository/centos7-centosplus
-sed -i '$ i gpgcheck=0' /etc/yum.repos.d/pnexus.sytes.net_repository_centos7-centosplus.repo
 yum-config-manager --add-repo http://pnexus.sytes.net/repository/centos7-extras
 sed -i '$ i gpgcheck=0' /etc/yum.repos.d/pnexus.sytes.net_repository_centos7-extras.repo
 yum-config-manager --add-repo http://pnexus.sytes.net/repository/centos7-os
@@ -12,4 +11,12 @@ yum-config-manager --add-repo http://pnexus.sytes.net/repository/centos7-updates
 sed -i '$ i gpgcheck=0' /etc/yum.repos.d/pnexus.sytes.net_repository_centos7-updates.repo
 yum-config-manager --add-repo http://pnexus.sytes.net/repository/epel
 sed -i '$ i gpgcheck=0' /etc/yum.repos.d/pnexus.sytes.net_repository_epel.repo
-sudo yum update -y
+yum-config-manager --add-repo http://pnexus.sytes.net/repository/docker-ce-stable
+sed -i '$ i gpgcheck=0' /etc/yum.repos.d/pnexus.sytes.net_repository_docker-ce-stable.repo
+yum-config-manager --add-repo http://pnexus.sytes.net/repository/google-chrome
+sed -i '$ i gpgcheck=0' /etc/yum.repos.d/pnexus.sytes.net_repository_google-chrome.repo
+yum-config-manager --add-repo http://pnexus.sytes.net/repository/openstack-rocky
+sed -i '$ i gpgcheck=0' /etc/yum.repos.d/pnexus.sytes.net_repository_openstack-rocky.repo
+
+yum update -y
+MANY
