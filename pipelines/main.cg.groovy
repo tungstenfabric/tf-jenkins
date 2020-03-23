e30fa4f46aa56a03c070073dc19eeaaf98ad10b6
Ivan Timofeev <ivanv.timofeev@gmail.com>
2020-03-23 14:29:32 +0300

    remove difficulties

--- pipelines/main.cg.groovy
+++ pipelines/main.cg.groovy
@@ -408,9 +408,7 @@ def gate_wait_for_fetch(build_no){
   println("DEBUG: Result of fetch job is : ${fetch_job.getResult()}")
 
   // Wait for fetch job finished
-  def result = true
-  while(result){
-    result = fetch_job.getResult().toString() == "null"
+  while(fetch_job.getResult().toString() == "null"){
     println("INFO: Waiting for fetch job will finished")
     try{
            sleep(20)
