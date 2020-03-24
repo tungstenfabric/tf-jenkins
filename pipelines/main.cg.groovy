0006c3c8a82cb95976827bf52e29ad0e36e23cef
Ivan Timofeev <ivanv.timofeev@gmail.com>
2020-03-24 16:23:30 +0300

    Save base build no

--- pipelines/main.cg.groovy
+++ pipelines/main.cg.groovy
@@ -77,6 +77,8 @@ timestamps {
         def fetch_sources_count = jobs.count { return it.value['job-name'] == 'fetch-sources' }
         println("DEBUG: There must be two fetch jobs: ${fetch_sources_count}")
 
+        def base_build_no = null
+
         if (env.GERRIT_PIPELINE == 'gate_concurrent'){
           println("DEBUG: Gate concurrent detect")
           while(true){
@@ -85,7 +87,7 @@ timestamps {
               // some gate builds are in process
               builds_map = create_gate_builds_map()
               println("DEBUG: prepare builds_map = ${builds_map} ")
-              set_devenv_tag(builds_map, fetch_sources_count)
+              base_build_no = set_devenv_tag(builds_map, fetch_sources_count)
               // Run jubs baset on DEVENVTAG if exists
               println("DEBUG: Just before starts jobs: ${jobs}")
               jobs_utils.run_jobs(jobs)
@@ -97,7 +99,6 @@ timestamps {
                 throw new Exception(ex)
               }
             }finally{
-              def base_build_no = get_base_build_no(BUILD_ID)
               if(base_build_no){
                 println("DEBUG: We are found base pipeline and waiting when base pipeline will finished")
                 wait_pipeline_finished(base_build_no)
@@ -329,8 +330,10 @@ def create_gate_builds_map(){
 
 // The function find base build in builds_map, wait for fetch-sources of base build is finished
 // and set DEVENVTAG to global.env
+// return null or build_no for base branch
 def set_devenv_tag(builds_map, fetch_sources_count){
   println("DEBUG: I'm at set_devenv_tag builds_map = ${builds_map}")
+  def res = null
   builds_map.any {
     def build = it.value
     def build_no = it.key
@@ -362,59 +365,16 @@ def set_devenv_tag(builds_map, fetch_sources_count){
           echo "export DEVENVTAG=${build['container_tag']}" >> global.env
         """
         archiveArtifacts(artifacts: 'global.env')
+        res =  build_no
         return true
       }
   }
+  return res
 }
 
 // Return true if branch matches product branch
 def is_branch_product(branch){
   return branch == "master" || branch ==~ /R19\d*/ || branch ==~ /R20\d*/ || branch ==~ /R21\d*/
-}
-
-
-// if DEVENVTAG is set for build_no, function find build which CONTAINER_TAG is same and return
-// build_id.
-// if DEVENVTAG not found for the build - return false
-def get_base_build_no(build_no){
-  def job = jenkins.model.Jenkins.instance.getItem('pipeline-gate-opencontrail-c')
-  // Get DEVENVTAG for build_no pipeline
-  def devenv_tag = null
-  def base_build = null
-  job.builds.any {
-    build = it
-      if (artifactManager.root().isDirectory()) {
-        def fileList = artifactManager.root().list()
-        fileList.each {
-          def file = it
-          if(file.toString().contains('global.env')) {
-            // extract global.env artifact for each build if exists
-            def fileText = it.open().getText()
-            fileText.split("\n").each {
-              def line = it
-              // Check if DEVENVTAG is set for build_no
-              if(line.contains('DEVENVTAG') &&
-                 build_no.toInteger() == build.getEnvVars().BUILD_ID.toInteger()) {
-                devenv_tag = line.split('=')[1].trim()
-              }
-              // We suppose we've met CONTRAIL_CONTAINER_TAG after we met DEVENVTAG and init devenv_tag
-              if(devenv_tag && line.contains('CONTRAIL_CONTAINER_TAG')){
-                def container_tag = line.split('=')[1].trim()
-                if(container_tag == devenv_tag){
-                  base_build = build.getEnvVars().BUILD_ID.toInteger()
-                  return true
-                }
-              }
-            }
-          }
-        }
-      }
-  }
-
-  // We not found DEVENVTAG for current build and return false
-  if(! devenv_tag || ! base_build)
-    return false
-  return base_build
 }
 
 // Function find the build with build_no and wait it finishes with any result
