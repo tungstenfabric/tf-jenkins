86584764857214928af9063c8aed6996864919f2
alexey-mr <alexey.morlang@gmail.com>
2020-03-27 16:36:39 +0300

    Added openshift job

---
+++ jobs/devstack/openshift/deploy_tf.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -eE
+set -o pipefail
+
+[ "${DEBUG,,}" == "true" ] && set -x
+
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname $my_file)"
+
+source "$my_dir/definitions"
+
+echo 'INFO: Deploy TF with Openshift'
+
+rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS" $WORKSPACE/src $IMAGE_SSH_USER@$instance_ip:./
+
+cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $IMAGE_SSH_USER@$instance_ip || res=1
+[ "${DEBUG,,}" == "true" ] && set -x
+export WORKSPACE=\$HOME
+export DEBUG=$DEBUG
+export CONTAINER_REGISTRY="$CONTAINER_REGISTRY"
+export CONTRAIL_CONTAINER_TAG="$CONTRAIL_CONTAINER_TAG$TAG_SUFFIX"
+export PATH=\$PATH:/usr/sbin
+cd src/tungstenfabric/tf-devstack/openshift
+ORCHESTRATOR=$ORCHESTRATOR ./run.sh
+EOF
+
+echo "INFO: Deploy TF finished"
+exit $res
