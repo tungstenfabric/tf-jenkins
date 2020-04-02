a230ffe865d566f8e5229d7e96e05ec00cdb85f7
Andrey Pavlov <andrey-mp@yandex.ru>
2020-04-02 21:17:18 +0300

    implement apllying patchsets for tf-test

---
+++ infra/gerrit/apply_patchsets.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+src_dir="$1"
+project_fqdn="$2"
+patchsets_info_file="$3"
+
+cd $src_dir/$project_fqdn
+for ref in $(cat info.json | jq -r --arg project $project_fqdn '.[] | select(.project == $project)' | .ref); do
+  echo "INFO: run 'git fetch $GERRIT_URL/$project_fqdn $ref && git cherry-pick FETCH_HEAD'"
+  git fetch $GERRIT_URL/$project_fqdn $ref && git cherry-pick FETCH_HEAD
+done
