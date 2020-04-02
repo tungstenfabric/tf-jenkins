56b1bf5052c3876a4acbf5b8b217d4af2b9f6623
Ivan Timofeev <ivanv.timofeev@gmail.com>
2020-04-02 18:36:42 +0300

    Remove extra debug

--- pipelines/main.cg2.groovy
+++ pipelines/main.cg2.groovy
@@ -43,8 +43,6 @@ timestamps {
         config_utils = load("${WORKSPACE}/tf-jenkins/pipelines/utils/config.groovy")
         jobs_utils = load("${WORKSPACE}/tf-jenkins/pipelines/utils/jobs.groovy")
         gate_utils = load("${WORKSPACE}/tf-jenkins/pipelines/utils/gate.groovy")
-        println("DEBUG: gate_utils has been loaded and it is : ${gate_utils}")
-        println("DEBUG: job_utils has been loaded and it is : ${jobs_utils}")
       }
       // TODO: remove comment here when gating is ready
       //if (env.GERRIT_PIPELINE == 'gate') { // && !gerrit_utils.has_gate_approvals()) {
