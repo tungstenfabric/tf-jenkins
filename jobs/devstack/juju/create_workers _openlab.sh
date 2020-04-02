206a7e7e8f379464ef036fe122c7b5c3706efa43
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-04-02 17:22:23 +0300

    Try spin vm in openlab2

--- jobs/devstack/juju/create_workers _openlab.sh
+++ jobs/devstack/juju/create_workers _openlab.sh
@@ -6,6 +6,17 @@ set -o pipefail
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
-source "$my_dir/definitions"
-
+#source "$my_dir/definitions"
+cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS -p 30002 -i ${OPENLAB2_SSH_KEY} ${OPENLAB2_USER_NAME}@openlab.tf-jenkins.progmaticlab.com || res=1
+[ "${DEBUG,,}" == "true" ] && set -x
+export BUILD_TAG=$BUILD_TAG
+export PATH=\$PATH:/usr/sbin
+virsh destroy vm-maas || /bin/true
+virsh undefine vm-maas || /bin/true
+rm -f vm-maas || /bin/true
+virt-clone --original ubuntu18 --name vm-maas --auto-clone --file vm-maas
+sleep 30
+echo "VM is spinned"
+EOF
+# exit $res
 exit 1
