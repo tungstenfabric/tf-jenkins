2c09efd9c243c7eadb96dc0bac78d4c61edb99ec
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-04-02 18:55:18 +0300

    Add check connect to vm

--- jobs/devstack/juju/create_workers_openlab.sh
+++ jobs/devstack/juju/create_workers_openlab.sh
@@ -6,22 +6,22 @@ set -o pipefail
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
-#source "$my_dir/definitions"
+
 
 ENV_FILE="$WORKSPACE/stackrc.$JOB_NAME.env"
 touch "$ENV_FILE"
 echo "export ENVIRONMENT_OS=ubuntu18" >> "$ENV_FILE"
 echo "export IMAGE_SSH_USER=jenkins" >> "$ENV_FILE"
-echo "export instance_ip=10.0.0.34" >> "$ENV_FILE"
+echo "export instance_ip=192.168.51.5" >> "$ENV_FILE"
 echo "export SSH_EXTRA_OPTIONS=$SSH_EXTRA_OPTIONS" >> "$ENV_FILE"
 
-cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS -p 30002 -i ${OPENLAB2_SSH_KEY} jenkins@openlab.tf-jenkins.progmaticlab.com || res=1
+cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS -p 30002 -i $OPENLAB2_SSH_KEY jenkins@openlab.tf-jenkins.progmaticlab.com || res=1
 [ "${DEBUG,,}" == "true" ] && set -x
 export BUILD_TAG=$BUILD_TAG
 export PATH=\$PATH:/usr/sbin
 virsh destroy vm-maas || /bin/true
 virsh undefine vm-maas || /bin/true
-rm -f vm-maas || /bin/true
+rm -f vm-maas
 virt-clone --original ubuntu18 --name vm-maas --auto-clone --file vm-maas
 virsh start vm-maas
 sleep 30
@@ -29,4 +29,10 @@ echo "VM is spinned"
 EOF
 exit $res
 
+source "$my_dir/definitions"
 
+timeout 300 bash -c "\
+while /bin/true ; do \
+  ssh -i -i $OPENLAB2_SSH_KEY $SSH_OPTIONS $IMAGE_SSH_USER@$instance_ip 'uname -a' && break ; \
+  sleep 10 ; \
+done"
