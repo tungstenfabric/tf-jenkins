a98fb34ac2cd699169728d86ed1e236ac34d80d0
Andrey Pavlov <andrey-mp@yandex.ru>
2020-04-03 09:35:58 +0300

    fix typo

--- infra/gerrit/apply_patchsets.sh
+++ infra/gerrit/apply_patchsets.sh
@@ -5,7 +5,7 @@ project_fqdn="$2"
 patchsets_info_file="$3"
 
 cd $src_dir/$project_fqdn
-for ref in $(cat info.json | jq -r --arg project $project_fqdn '.[] | select(.project == $project)' | .ref); do
+for ref in $(cat $patchsets_info_file | jq -r --arg project $project_fqdn '.[] | select(.project == $project)' | .ref); do
   echo "INFO: run 'git fetch $GERRIT_URL/$project_fqdn $ref && git cherry-pick FETCH_HEAD'"
   git fetch $GERRIT_URL/$project_fqdn $ref && git cherry-pick FETCH_HEAD
 done
