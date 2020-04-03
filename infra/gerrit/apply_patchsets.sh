2d3cbc92a755cc49c7d0a153f07235cb0341ec56
Andrey Pavlov <andrey-mp@yandex.ru>
2020-04-03 10:14:36 +0300

    configure git before using

--- infra/gerrit/apply_patchsets.sh
+++ infra/gerrit/apply_patchsets.sh
@@ -4,6 +4,9 @@ src_dir="$(readlink -e $1)"
 project_fqdn="$2"
 patchsets_info_file="$(readlink -e $3)"
 
+git config --get user.name >/dev/null  2>&1 || git config --global user.name "tf-jenkins"
+git config --get user.email >/dev/null 2>&1 || git config --global user.email "tf-jenkins@tf"
+
 cd $src_dir/$project_fqdn
 for ref in $(cat $patchsets_info_file | jq -r --arg project $project_fqdn '.[] | select(.project == $project) | .ref'); do
   echo "INFO: run 'git fetch $GERRIT_URL/$project_fqdn $ref && git cherry-pick FETCH_HEAD'"
