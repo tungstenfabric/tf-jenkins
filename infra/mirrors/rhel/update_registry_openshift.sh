ba0e510b59e8631d2aeba62233d68db960e6d796
alexey-mr <alexey.morlang@gmail.com>
2020-04-07 22:03:22 +0300

    Update script for openshift regsitry mirror sync

--- infra/mirrors/rhel/update_registry_openshift.sh
+++ infra/mirrors/rhel/update_registry_openshift.sh
@@ -12,7 +12,12 @@ REDHAT_REGISTRY="registry.redhat.io"
 
 RHEL_NAMESPACE="rhel7"
 OPENSHIFT_NAMESPACE="openshift3"
-TAG='v3.11'
+TAG='v3.11.188'
+
+#docker
+docker_containers=( \
+  redis:4.0.2 \
+)
 
 #rhel
 rhel_containers=( \
@@ -21,6 +26,7 @@ rhel_containers=( \
 
 #openshift
 openshift_containers=( \
+  grafana \
   logging-auth-proxy \
   logging-curator \
   logging-deployer \
@@ -31,20 +37,43 @@ openshift_containers=( \
   metrics-deployer \
   metrics-hawkular-metrics \
   metrics-heapster \
+  metrics-schema-installer \
   node \
+  oauth-proxy \
   openvswitch \
   ose \
+  ose-ansible-service-broker \
   ose-cluster-monitoring-operator \
+  ose-configmap-reloader \
+  ose-console \
   ose-control-plane \
   ose-deployer \
   ose-docker-builder \
   ose-docker-registry \
   ose-haproxy-router \
+  ose-kube-rbac-proxy \
+  ose-kube-state-metrics \
+  ose-logging-elasticsearch5 \
+  ose-logging-fluentd \
+  ose-logging-kibana5 \
+  ose-metrics-server \
   ose-node \
   ose-pod \
+  ose-prometheus-config-reloader \
+  ose-prometheus-operator \
   ose-recycler \
+  ose-service-catalog \
   ose-sti-builder \
+  ose-template-service-broker \
+  ose-web-console \
+  prometheus \
+  prometheus-alertmanager \
+  prometheus-node-exporter \
   registry-console \
+)
+
+openshift_containers_2=( \
+  ose-deployer:v3.11.200 \
 )
 
 function sync_container() {
@@ -56,24 +85,28 @@ function sync_container() {
 }
 
 if [[ -n "$RHEL_USER" && "$RHEL_PASSWORD" ]] ; then
+  echo "INFO: logi to docker registry $REDHAT_REGISTRY"
   sudo docker login --username $RHEL_USER --password $RHEL_PASSWORD $REDHAT_REGISTRY || {
     echo "ERROR: failed to login "
   }
 fi
 
-all_containers=$(printf "${RHEL_NAMESPACE}/%s " "${rhel_containers[@]}")
+all_containers+=$(printf "%s " "${docker_containers[@]}")
+all_containers+=$(printf "${RHEL_NAMESPACE}/%s " "${rhel_containers[@]}")
 all_containers+=$(printf "${OPENSHIFT_NAMESPACE}/%s:$TAG " "${openshift_containers[@]}")
-jobs=""
+all_containers+=$(printf "${RHEL_NAMESPACE}/%s " "${openshift_containers_2[@]}")
+
+# jobs=""
+res=0
 for c in ${all_containers} ; do  
   echo "INFO: start sync $c"
-  sync_container $c &
-  jobs+=" $!"
+  sync_container $c || res=1
+  # jobs+=" $!"
 done
 
-res=0
-for j in $jobs ; do
-  wait $j || res=1
-done
+# for j in $jobs ; do
+#   wait $j || res=1
+# done
 
 if [[ $res != 0 ]] ; then
   echo "ERROR: sync failed"
