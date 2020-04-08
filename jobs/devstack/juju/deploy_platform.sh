44aca7b360831d1e8c43f58c0ab21562e7f9933e
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-04-08 12:57:03 +0300

    Try use sub-shell

--- jobs/devstack/juju/deploy_platform.sh
+++ jobs/devstack/juju/deploy_platform.sh
@@ -9,18 +9,9 @@ my_dir="$(dirname $my_file)"
 source "$my_dir/definitions"
 
 echo "INFO: Deploy platform for $JOB_NAME"
-echo "$RSYNC_EXTRA_OPTIONS"
 rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $RSYNC_EXTRA_OPTIONS" $WORKSPACE/src $IMAGE_SSH_USER@$instance_ip:./
-echo "$SSH_EXTRA_OPTIONS"
 
-timeout 300 bash -c "\
-while /bin/true ; do \
-  ssh -i $OPENLAB2_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS $IMAGE_SSH_USER@$instance_ip 'uname -a' && break ; \
-  sleep 10 ; \
-done"
-
-bash -c "ssh -i $OPENLAB2_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS $IMAGE_SSH_USER@$instance_ip 'hostname'"
-
+bash -c "\
 cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS $IMAGE_SSH_USER@$instance_ip || res=1
 [ "${DEBUG,,}" == "true" ] && set -x
 export WORKSPACE=\$HOME
@@ -31,7 +22,7 @@ export CONTRAIL_CONTAINER_TAG="$CONTRAIL_CONTAINER_TAG$TAG_SUFFIX"
 export PATH=\$PATH:/usr/sbin
 cd src/tungstenfabric/tf-devstack/juju
 ORCHESTRATOR=$ORCHESTRATOR CLOUD=local ./run.sh platform
-EOF
+EOF"
 
 echo "INFO: Deploy platform finished"
 exit $res
