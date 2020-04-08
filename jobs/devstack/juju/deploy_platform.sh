33dc5be204bfd8f29d0c09ba01ee05bb70d59281
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-04-08 12:39:15 +0300

    Set debug

--- jobs/devstack/juju/deploy_platform.sh
+++ jobs/devstack/juju/deploy_platform.sh
@@ -19,7 +19,7 @@ while /bin/true ; do \
   sleep 10 ; \
 done"
 
-ssh -i $OPENLAB2_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS $IMAGE_SSH_USER@$instance_ip 'hostname'
+bash -c "ssh -i $OPENLAB2_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS $IMAGE_SSH_USER@$instance_ip 'hostname'"
 
 cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS $IMAGE_SSH_USER@$instance_ip || res=1
 [ "${DEBUG,,}" == "true" ] && set -x
