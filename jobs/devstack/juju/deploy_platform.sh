a0c266c049e6cc38ba4b5121ed59ef462defd6b0
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-04-08 13:05:40 +0300

    Try use subshell for rsync

--- jobs/devstack/juju/deploy_platform.sh
+++ jobs/devstack/juju/deploy_platform.sh
@@ -9,7 +9,7 @@ my_dir="$(dirname $my_file)"
 source "$my_dir/definitions"
 
 echo "INFO: Deploy platform for $JOB_NAME"
-rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $RSYNC_EXTRA_OPTIONS" $WORKSPACE/src $IMAGE_SSH_USER@$instance_ip:./
+bash -c "rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS" $WORKSPACE/src $IMAGE_SSH_USER@$instance_ip:./"
 
 bash -c "\
 cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS $IMAGE_SSH_USER@$instance_ip || res=1
@@ -22,7 +22,6 @@ export CONTRAIL_CONTAINER_TAG="$CONTRAIL_CONTAINER_TAG$TAG_SUFFIX"
 export PATH=\$PATH:/usr/sbin
 cd src/tungstenfabric/tf-devstack/juju
 ORCHESTRATOR=$ORCHESTRATOR CLOUD=local ./run.sh platform
-EOF"
-
+EOF
 echo "INFO: Deploy platform finished"
-exit $res
+exit $res"
