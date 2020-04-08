959472dfeca011dd4362bebb0cd111b5c6328b84
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-04-08 13:11:13 +0300

    Revert rsync changes

--- jobs/devstack/juju/deploy_platform.sh
+++ jobs/devstack/juju/deploy_platform.sh
@@ -9,7 +9,7 @@ my_dir="$(dirname $my_file)"
 source "$my_dir/definitions"
 
 echo "INFO: Deploy platform for $JOB_NAME"
-bash -c "rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS" $WORKSPACE/src $IMAGE_SSH_USER@$instance_ip:./"
+rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS" $WORKSPACE/src $IMAGE_SSH_USER@$instance_ip:./
 
 bash -c "\
 cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS $IMAGE_SSH_USER@$instance_ip || res=1
