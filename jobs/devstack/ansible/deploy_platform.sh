e08516272e2be6cc13d9b4a71f6d1e6ce39c0a7a
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-04-08 11:03:57 +0300

    Fix changes to the wrong folder

--- jobs/devstack/ansible/deploy_platform.sh
+++ jobs/devstack/ansible/deploy_platform.sh
@@ -2,7 +2,7 @@
 set -o pipefail
 
 [ "${DEBUG,,}" == "true" ] && set -x
-set -x
+
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
@@ -12,28 +12,17 @@ echo "INFO: Deploy platform for $JOB_NAME"
 
 rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS" $WORKSPACE/src $IMAGE_SSH_USER@$instance_ip:./
 
-ssh -v -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS $IMAGE_SSH_USER@$instance_ip \
-    [ "${DEBUG,,}" == "true" ] && set -x; \
-    export WORKSPACE=\$HOME; \
-    export DEBUG=$DEBUG; \
-    export OPENSTACK_VERSION=$OPENSTACK_VERSION; \
-    export CONTAINER_REGISTRY="$CONTAINER_REGISTRY"; \
-    export CONTRAIL_CONTAINER_TAG="$CONTRAIL_CONTAINER_TAG$TAG_SUFFIX"; \
-    export PATH=\$PATH:/usr/sbin; \
-    cd src/tungstenfabric/tf-devstack/ansible; \
-    "ORCHESTRATOR=$ORCHESTRATOR ./run.sh platform" || res=1
-
-#cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $IMAGE_SSH_USER@$instance_ip || res=1
-#[ "${DEBUG,,}" == "true" ] && set -x
-#export WORKSPACE=\$HOME
-#export DEBUG=$DEBUG
-#export OPENSTACK_VERSION=$OPENSTACK_VERSION
-#export CONTAINER_REGISTRY="$CONTAINER_REGISTRY"
-#export CONTRAIL_CONTAINER_TAG="$CONTRAIL_CONTAINER_TAG$TAG_SUFFIX"
-#export PATH=\$PATH:/usr/sbin
-#cd src/tungstenfabric/tf-devstack/ansible
-#ORCHESTRATOR=$ORCHESTRATOR ./run.sh platform
-#EOF
+cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $IMAGE_SSH_USER@$instance_ip || res=1
+[ "${DEBUG,,}" == "true" ] && set -x
+export WORKSPACE=\$HOME
+export DEBUG=$DEBUG
+export OPENSTACK_VERSION=$OPENSTACK_VERSION
+export CONTAINER_REGISTRY="$CONTAINER_REGISTRY"
+export CONTRAIL_CONTAINER_TAG="$CONTRAIL_CONTAINER_TAG$TAG_SUFFIX"
+export PATH=\$PATH:/usr/sbin
+cd src/tungstenfabric/tf-devstack/ansible
+ORCHESTRATOR=$ORCHESTRATOR ./run.sh platform
+EOF
 
 echo "INFO: Deploy platform finished"
 exit $res
--- jobs/devstack/ansible/deploy_tf.sh
+++ jobs/devstack/ansible/deploy_tf.sh
@@ -12,28 +12,17 @@ echo 'INFO: Deploy TF for ansible-deployer'
 
 rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS" $WORKSPACE/src $IMAGE_SSH_USER@$instance_ip:./
 
-ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS $IMAGE_SSH_USER@$instance_ip \
-    [ "${DEBUG,,}" == "true" ] && set -x; \
-    export WORKSPACE=\$HOME; \
-    export DEBUG=$DEBUG; \
-    export OPENSTACK_VERSION=$OPENSTACK_VERSION; \
-    export CONTAINER_REGISTRY="$CONTAINER_REGISTRY"; \
-    export CONTRAIL_CONTAINER_TAG="$CONTRAIL_CONTAINER_TAG$TAG_SUFFIX"; \
-    export PATH=\$PATH:/usr/sbin; \
-    cd src/tungstenfabric/tf-devstack/ansible; \
-    ORCHESTRATOR=$ORCHESTRATOR ./run.sh || res=1
-
-#cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $IMAGE_SSH_USER@$instance_ip || res=1
-#[ "${DEBUG,,}" == "true" ] && set -x
-#export WORKSPACE=\$HOME
-#export DEBUG=$DEBUG
-#export OPENSTACK_VERSION=$OPENSTACK_VERSION
-#export CONTAINER_REGISTRY="$CONTAINER_REGISTRY"
-#export CONTRAIL_CONTAINER_TAG="$CONTRAIL_CONTAINER_TAG$TAG_SUFFIX"
-#export PATH=\$PATH:/usr/sbin
-#cd src/tungstenfabric/tf-devstack/ansible
-#ORCHESTRATOR=$ORCHESTRATOR ./run.sh
-#EOF
+cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $IMAGE_SSH_USER@$instance_ip || res=1
+[ "${DEBUG,,}" == "true" ] && set -x
+export WORKSPACE=\$HOME
+export DEBUG=$DEBUG
+export OPENSTACK_VERSION=$OPENSTACK_VERSION
+export CONTAINER_REGISTRY="$CONTAINER_REGISTRY"
+export CONTRAIL_CONTAINER_TAG="$CONTRAIL_CONTAINER_TAG$TAG_SUFFIX"
+export PATH=\$PATH:/usr/sbin
+cd src/tungstenfabric/tf-devstack/ansible
+ORCHESTRATOR=$ORCHESTRATOR ./run.sh
+EOF
 
 echo "INFO: Deploy TF finished"
 exit $res
