0a1d361a939d6d45d1ba78ac0131149a1cf60a2e
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-04-09 23:25:01 +0300

    Keep backslash

--- jobs/test/sanity/run.sh
+++ jobs/test/sanity/run.sh
@@ -16,8 +16,8 @@ bash -c "\
 ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS $IMAGE_SSH_USER@$instance_ip \
 [ "${DEBUG,,}" == "true" ] && set -x; \
 export DEBUG=$DEBUG; \
-export PATH=\$PATH:/usr/sbin; \
-export WORKSPACE=\$HOME; \
+export PATH=\\\$PATH:/usr/sbin; \
+export WORKSPACE=\\\$HOME; \
 export CONTAINER_REGISTRY="$CONTAINER_REGISTRY"; \
 export CONTRAIL_CONTAINER_TAG="$CONTRAIL_CONTAINER_TAG$TAG_SUFFIX"; \
 cd src/tungstenfabric/tf-test/contrail-sanity; \
