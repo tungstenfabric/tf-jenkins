327c4f859792252d762bbc87d8a472663223ed49
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-04-09 23:28:48 +0300

    Keep backslash

--- jobs/devstack/juju/deploy_tf.sh
+++ jobs/devstack/juju/deploy_tf.sh
@@ -17,14 +17,14 @@ rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS" $WORKSPACE/
 bash -c "\
 ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS $IMAGE_SSH_USER@$instance_ip \
 [ "${DEBUG,,}" == "true" ] && set -x; \
-export WORKSPACE=\\\$HOME; \
+export WORKSPACE=\\$HOME; \
 export DEBUG=$DEBUG; \
 export OPENSTACK_VERSION=$OPENSTACK_VERSION; \
 export CONTAINER_REGISTRY="$CONTAINER_REGISTRY"; \
 export CONTRAIL_CONTAINER_TAG="$CONTRAIL_CONTAINER_TAG$TAG_SUFFIX"; \
-export PATH=\\\$PATH:/usr/sbin; \
+export PATH=\\$PATH:/usr/sbin; \
 export CLOUD=$CLOUD; \
-source \\\$HOME/$CLOUD.vars || /bin/true; \
+source \\$HOME/$CLOUD.vars || /bin/true; \
 cd src/tungstenfabric/tf-devstack/juju; \
 ORCHESTRATOR=$ORCHESTRATOR ./run.sh" || ret=1
 echo "INFO: Deploy tf finished"
