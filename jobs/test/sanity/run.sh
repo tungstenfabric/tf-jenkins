b44fad903d4991b82996a7613f113367b9deb478
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-04-09 23:38:51 +0300

    Come back to heredoc

--- jobs/test/sanity/run.sh
+++ jobs/test/sanity/run.sh
@@ -13,15 +13,16 @@ rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS" $WORKSPACE/
 echo "INFO: Test sanity started"
 
 bash -c "\
-ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS $IMAGE_SSH_USER@$instance_ip \
-[ "${DEBUG,,}" == "true" ] && set -x; \
-export DEBUG=$DEBUG; \
-export PATH=\\$PATH:/usr/sbin; \
-export WORKSPACE=\\$HOME; \
-export CONTAINER_REGISTRY="$CONTAINER_REGISTRY"; \
-export CONTRAIL_CONTAINER_TAG="$CONTRAIL_CONTAINER_TAG$TAG_SUFFIX"; \
-cd src/tungstenfabric/tf-test/contrail-sanity; \
-ORCHESTRATOR=$ORCHESTRATOR ./run.sh" || res=1
-echo "INFO: Test sanity finished"
+cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS $IMAGE_SSH_USER@$instance_ip
+[ "${DEBUG,,}" == "true" ] && set -x
+export DEBUG=$DEBUG
+export PATH=\\\$PATH:/usr/sbin
+export WORKSPACE=\\\$HOME
+export CONTAINER_REGISTRY="$CONTAINER_REGISTRY"
+export CONTRAIL_CONTAINER_TAG="$CONTRAIL_CONTAINER_TAG$TAG_SUFFIX"
+cd src/tungstenfabric/tf-test/contrail-sanity
+ORCHESTRATOR=$ORCHESTRATOR ./run.sh || res=1
+echo 'INFO Test sanity finished'
 exit $res
-
+EOF
+"
