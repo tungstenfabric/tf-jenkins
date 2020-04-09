2deedb4a6ad5063b80e19c6997c60eb07b011deb
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-04-09 23:22:45 +0300

    Keep backslash

--- jobs/devstack/juju/deploy_platform.sh
+++ jobs/devstack/juju/deploy_platform.sh
@@ -14,15 +14,15 @@ rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS" $WORKSPACE/
 bash -c "\
 ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS $IMAGE_SSH_USER@$instance_ip \
 [ "${DEBUG,,}" == "true" ] && set -x; \
-export WORKSPACE=\$HOME; \
+export WORKSPACE=\\\$HOME; \
 export DEBUG=$DEBUG; \
 export OPENSTACK_VERSION=$OPENSTACK_VERSION; \
 export CONTAINER_REGISTRY="$CONTAINER_REGISTRY"; \
 export CONTRAIL_CONTAINER_TAG="$CONTRAIL_CONTAINER_TAG$TAG_SUFFIX"; \
 export CLOUD=${CLOUD:-"local"}; \
-source \$HOME/$CLOUD.vars || /bin/true; \
-export PATH=\$PATH:/usr/sbin; \
-cd \$HOME/src/tungstenfabric/tf-devstack/juju; \
+source \\\$HOME/$CLOUD.vars || /bin/true; \
+export PATH=\\\$PATH:/usr/sbin; \
+cd \\\$HOME/src/tungstenfabric/tf-devstack/juju; \
 ORCHESTRATOR=$ORCHESTRATOR ./run.sh platform" || ret=1
 
 echo "INFO: Deploy platform finished"
--- jobs/devstack/juju/deploy_tf.sh
+++ jobs/devstack/juju/deploy_tf.sh
@@ -15,19 +15,17 @@ CLOUD=${CLOUD:-"local"}
 rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS" $WORKSPACE/src $IMAGE_SSH_USER@$instance_ip:./
 
 bash -c "\
-cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS $IMAGE_SSH_USER@$instance_ip
-[ "${DEBUG,,}" == "true" ] && set -x
-export WORKSPACE=\$HOME
-export DEBUG=$DEBUG
-export OPENSTACK_VERSION=$OPENSTACK_VERSION
-export CONTAINER_REGISTRY="$CONTAINER_REGISTRY"
-export CONTRAIL_CONTAINER_TAG="$CONTRAIL_CONTAINER_TAG$TAG_SUFFIX"
-export PATH=\$PATH:/usr/sbin
-export CLOUD=$CLOUD
-source \$HOME/$CLOUD.vars || /bin/true
-cd src/tungstenfabric/tf-devstack/juju
-ORCHESTRATOR=$ORCHESTRATOR ./run.sh || ret=1
-echo "INFO - Deploy tf finished"
-exit \$ret
-EOF
-"
+ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS $IMAGE_SSH_USER@$instance_ip \
+[ "${DEBUG,,}" == "true" ] && set -x; \
+export WORKSPACE=\\\$HOME; \
+export DEBUG=$DEBUG; \
+export OPENSTACK_VERSION=$OPENSTACK_VERSION; \
+export CONTAINER_REGISTRY="$CONTAINER_REGISTRY"; \
+export CONTRAIL_CONTAINER_TAG="$CONTRAIL_CONTAINER_TAG$TAG_SUFFIX"; \
+export PATH=\\\$PATH:/usr/sbin; \
+export CLOUD=$CLOUD; \
+source \\\$HOME/$CLOUD.vars || /bin/true; \
+cd src/tungstenfabric/tf-devstack/juju; \
+ORCHESTRATOR=$ORCHESTRATOR ./run.sh" || ret=1
+echo "INFO: Deploy tf finished"
+exit $ret
