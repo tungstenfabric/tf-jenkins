1c72ecbcc7cf65bb19319c4c47ee4a7f62286bfd
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-04-09 14:37:12 +0300

    Revert deploy maas to deploy platform

--- jobs/devstack/juju/create_workers_openlab.sh
+++ jobs/devstack/juju/create_workers_openlab.sh
@@ -32,14 +32,3 @@ while /bin/true ; do \
   ssh -i $OPENLAB2_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS $IMAGE_SSH_USER@$instance_ip 'uname -a' && break ; \
   sleep 10 ; \
 done"
-
-if [ "$CLOUD" == "maas" ]; then
-bash -c "\
-cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS $IMAGE_SSH_USER@$instance_ip
-[ "${DEBUG,,}" == "true" ] && set -x
-export IPMI_IPS='192.168.51.20 192.168.51.21 192.168.51.22 192.168.51.23 192.168.51.24'
-cd \$HOME/src/tungstenfabric/tf-devstack/common
-./deploy_maas.sh | grep ^export > \$HOME/maas.vars
-EOF
-"
-fi
--- jobs/devstack/juju/deploy_tf.sh
+++ jobs/devstack/juju/deploy_tf.sh
@@ -13,6 +13,18 @@ echo 'INFO: Deploy TF with juju'
 CLOUD=${CLOUD:-"local"}
 
 rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS" $WORKSPACE/src $IMAGE_SSH_USER@$instance_ip:./
+
+if [ "$CLOUD" == "maas" ]; then
+bash -c "\
+cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS $IMAGE_SSH_USER@$instance_ip
+[ "${DEBUG,,}" == "true" ] && set -x
+export IPMI_IPS='192.168.51.20 192.168.51.21 192.168.51.22 192.168.51.23 192.168.51.24'
+cd \$HOME/src/tungstenfabric/tf-devstack/common
+./deploy_maas.sh | grep ^export > \$HOME/maas.vars
+EOF
+"
+fi
+
 bash -c "\
 cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS $IMAGE_SSH_USER@$instance_ip
 [ "${DEBUG,,}" == "true" ] && set -x
