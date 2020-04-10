b26dc94fd8c63ef6cf196cbd638171bfe5af3db3
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-04-10 15:03:16 +0300

    Try to copy&execute script on remote host

--- jobs/devstack/juju/deploy_platform.sh
+++ jobs/devstack/juju/deploy_platform.sh
@@ -9,22 +9,26 @@ my_dir="$(dirname $my_file)"
 source "$my_dir/definitions"
 
 echo "INFO: Deploy platform for $JOB_NAME"
-rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS" $WORKSPACE/src $IMAGE_SSH_USER@$instance_ip:./
-
-bash -c "\
-cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS $IMAGE_SSH_USER@$instance_ip
+deploy_platform.sh <<EOF
+#!/bin/bash
 [ "${DEBUG,,}" == "true" ] && set -x
-export WORKSPACE=\\\$HOME
+export WORKSPACE=\$HOME
 export DEBUG=$DEBUG
 export OPENSTACK_VERSION=$OPENSTACK_VERSION
 export CONTAINER_REGISTRY="$CONTAINER_REGISTRY"
 export CONTRAIL_CONTAINER_TAG="$CONTRAIL_CONTAINER_TAG$TAG_SUFFIX"
 export CLOUD=${CLOUD:-"local"}
-source \\\$HOME/$CLOUD.vars || /bin/true
-export PATH=\\\$PATH:/usr/sbin
-cd \\\$HOME/src/tungstenfabric/tf-devstack/juju
+source \$HOME/$CLOUD.vars || /bin/true
+export PATH=\$PATH:/usr/sbin
+cd \$HOME/src/tungstenfabric/tf-devstack/juju
 ORCHESTRATOR=$ORCHESTRATOR ./run.sh platform || ret=1
 echo "INFO Deploy platform finished"
-exit \\\$ret
+exit \$ret
 EOF
-"
+
+rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS" {$WORKSPACE/src,deploy_platform.sh} $IMAGE_SSH_USER@$instance_ip:./
+
+bash -c "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS $IMAGE_SSH_USER@$instance_ip deploy_platform.sh" || ret=1
+
+echo "INFO Deploy platform finished"
+exit $ret
