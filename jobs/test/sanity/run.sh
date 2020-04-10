bc546d680d82fbbe0ddeaf6c8dda8d0721b3c106
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-04-10 17:22:21 +0300

    Rework  remote test execution

--- jobs/test/sanity/run.sh
+++ jobs/test/sanity/run.sh
@@ -12,17 +12,18 @@ rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS" $WORKSPACE/
 
 echo "INFO: Test sanity started"
 
-bash -c "\
-cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS $IMAGE_SSH_USER@$instance_ip
+cat << EOF > $WORKSPACE/run_sanity_tests.sh
 [ "${DEBUG,,}" == "true" ] && set -x
 export DEBUG=$DEBUG
-export PATH=\\\$PATH:/usr/sbin
-export WORKSPACE=\\\$HOME
+export PATH=\$PATH:/usr/sbin
+export WORKSPACE=\$HOME
 export CONTAINER_REGISTRY="$CONTAINER_REGISTRY"
 export CONTRAIL_CONTAINER_TAG="$CONTRAIL_CONTAINER_TAG$TAG_SUFFIX"
 cd src/tungstenfabric/tf-test/contrail-sanity
-ORCHESTRATOR=$ORCHESTRATOR ./run.sh || res=1
-echo 'INFO Test sanity finished'
-exit \\\$res
+ORCHESTRATOR=$ORCHESTRATOR ./run.sh
 EOF
-"
+
+rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS" $WORKSPACE/run_sanity_tests.sh $IMAGE_SSH_USER@$instance_ip:./
+bash -c "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS $IMAGE_SSH_USER@$instance_ip 'bash ./run_sanity_tests.sh'" || res=1
+echo "INFO: Test sanity finished"
+exit $res
--- jobs/test/smoke/run.sh
+++ jobs/test/smoke/run.sh
@@ -12,11 +12,10 @@ rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS" $WORKSPACE/
 
 echo "INFO: Test smoke started"
 
-bash -c "\
-cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS $IMAGE_SSH_USER@$instance_ip
+cat << EOF > $WORKSPACE/run_smoke_test.sh
 [ "${DEBUG,,}" == "true" ] && set -x
 export DEBUG=$DEBUG
-export PATH=\\\$PATH:/usr/sbin
+export PATH=\$PATH:/usr/sbin
 
 printf '%*s\n' 120 | tr ' ' '='
 sudo contrail-status
@@ -28,12 +27,15 @@ printf '%*s\n' 120 | tr ' ' '*'
 ps ax -H
 printf '%*s\n' 120 | tr ' ' '*'
 
-export WORKSPACE=\\\$HOME
+export WORKSPACE=\$HOME
 export CONTAINER_REGISTRY="$CONTAINER_REGISTRY"
 export CONTRAIL_CONTAINER_TAG="$CONTRAIL_CONTAINER_TAG$TAG_SUFFIX"
 cd src/tungstenfabric/tf-test/smoke-test
-ORCHESTRATOR=$ORCHESTRATOR ./run.sh || res=1
-echo 'INFO - Test smoke finished'
-exit \\\$res
+ORCHESTRATOR=$ORCHESTRATOR ./run.sh
 EOF
-"
+
+rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS" $WORKSPACE/run_smoke_test.sh $IMAGE_SSH_USER@$instance_ip:./
+bash -c "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS $IMAGE_SSH_USER@$instance_ip 'bash ./run_smoke_test.sh'" || res=1
+
+echo "INFO: Test smoke finished"
+exit $res
