a4f1e8e327022ff61440dc9bac52b240c96e7498
alexey-mr <alexey.morlang@gmail.com>
2020-04-16 00:49:40 +0300

    Fixe ssh user for rhosp

--- jobs/devstack/rhosp/create_workers.sh
+++ jobs/devstack/rhosp/create_workers.sh
@@ -14,7 +14,7 @@ create_env_file="$WORKSPACE/stackrc.$JOB_NAME.env"
 echo "export OS_REGION_NAME=${OS_REGION_NAME}" > "$create_env_file"
 
 IMAGE_SSH_USER=${OS_IMAGE_USERS["${ENVIRONMENT_OS^^}"]}
-echo "export IMAGE_SSH_USER=$IMAGE_SSH_USER" >> "$create_env_file"
+echo "export SSH_USER=$IMAGE_SSH_USER" >> "$create_env_file"
 
 # wait for free resource
 while true; do
