667f12828a3ffe52d69e8dcb5330136b72bc0672
alexey-mr <alexey.morlang@gmail.com>
2020-04-15 21:00:11 +0300

    Source stackrc for rhosp cleanup

--- jobs/devstack/rhosp/remove_workers.sh
+++ jobs/devstack/rhosp/remove_workers.sh
@@ -6,6 +6,10 @@ set -o pipefail
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
+source "$my_dir/definitions"
+create_env_file="stackrc.$JOB_NAME.env"
+source $create_env_file
+
 cd src/tungstenfabric/tf-devstack/rhosp/providers/vexx
 ./cleanup.sh
 
