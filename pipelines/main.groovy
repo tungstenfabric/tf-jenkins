4919cd0c1136e424d65ac69cd5bee99067d586ca
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-04-20 13:33:22 +0300

    Fix msg

--- pipelines/main.groovy
+++ pipelines/main.groovy
@@ -251,7 +251,7 @@ def terminate_dependency(change_id) {
         dependent_changes << d_change
         rb.doStop()
         println('Try send msg')
-        def msg = "Build ${build} has been aborted when a new upstream pipeline is started"
+        def msg = "Build has been aborted when a new upstream pipeline is started"
         _notify_gerrit(msg, verified=0, submit=false, GERRIT_CHANGE_ID=d_change, GERRIT_PATCHSET_NUMBER=d_patchset, GERRIT_BRANCH=d_branch)
       }
     }
