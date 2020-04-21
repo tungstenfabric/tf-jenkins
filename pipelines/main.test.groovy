d813f11233c2b446f062904f49c654407119b1b2
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-04-21 17:25:02 +0300

    Set debug

--- pipelines/main.test.groovy
+++ pipelines/main.test.groovy
@@ -263,16 +263,16 @@ def terminate_dependency(change_id) {
       println('Dependent change:' + " " + target_change)
       dependent_changes += target_change
       // build.doStop()
-/*       try {
+       try {
         def msg = """Dependent build was started ${BUILD_URL}. This build has been aborted"""
-        gerrit_utils.notify_gerrit(msg, target_patchset, target_change, target_branch)
+      //  gerrit_utils.notify_gerrit(msg, target_patchset, target_change, target_branch)
       } catch (err) {
         println("Failed to provide comment to gerrit")
         def msg = err.getMessage()
         if (msg != null) {
           println(msg)
         }
-      } */
+      }
     }
   }
   return dependent_changes
