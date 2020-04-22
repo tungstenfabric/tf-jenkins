7891729b0b04d440731e2f4433feb7b8d8f35128
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-04-22 14:51:53 +0300

    Fix method

--- pipelines/main.test.groovy
+++ pipelines/main.test.groovy
@@ -284,10 +284,10 @@ def terminate_dependency(change_id) {
 def terminate_dependencies_runs(gerrit_change) {
   println('Search for dependent builds')
   def change_ids = terminate_dependency(gerrit_change)
-  if ( change_ids.size() > 0 ) {
-    for (change_id in change_ids) {
+  if (!change_ids)
+    continue
+  for (change_id in change_ids) {
       terminate_dependencies_runs(change_id)
-    }
   }
 }
 
