d59e6d65689696dfac30ef972bb4949f2bfffd0a
Andrey Pavlov <andrey-mp@yandex.ru>
2020-04-23 12:13:35 +0300

    clean logs directory for UT before collecting new

--- jobs/test/unit/collect_logs.sh
+++ jobs/test/unit/collect_logs.sh
@@ -17,7 +17,8 @@ tar -czf logs.tgz -C \$WORKSPACE/contrail/output logs || /bin/true
 EOF
 
 rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS" $IMAGE_SSH_USER@$instance_ip:logs.tgz $WORKSPACE/
-tar -zxf  $WORKSPACE/logs.tgz 
+rm -rf $WORKSPACE/logs
+tar -zxf $WORKSPACE/logs.tgz 
 ssh -i ${LOGS_HOST_SSH_KEY} ${SSH_OPTIONS} ${LOGS_HOST_USERNAME}@${LOGS_HOST} "mkdir -p ${FULL_LOGS_PATH}"
 rsync -a -e "ssh -i ${LOGS_HOST_SSH_KEY} ${SSH_OPTIONS}" ${WORKSPACE}/logs ${LOGS_HOST_USERNAME}@${LOGS_HOST}:${FULL_LOGS_PATH} || /bin/true
 
