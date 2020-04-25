f450e7b18788ccf3f2ff762cf7c24e0b686ff9b2
Andrey Pavlov <andrey-mp@yandex.ru>
2020-04-25 21:34:09 +0300

    implement UT targets filtering

--- jobs/test/unit/filter_targets.sh
+++ jobs/test/unit/filter_targets.sh
@@ -3,8 +3,7 @@ set -o pipefail
 
 [ "${DEBUG,,}" == "true" ] && set -x
 
-my_file="$(readlink -e "$0")"
-my_dir="$(dirname $my_file)"
+my_dir="$(realpath $(dirname "$0"))"
 
 source "$my_dir/definitions"
 
@@ -18,14 +17,56 @@ if [[ -z "TARGET_SET" ]]; then
   exit
 fi
 
+targets_file="$WORKSPACE/unittest_targets.lst"
+rm -f "$targets_file"
+touch "$targets_file"
+
 # hardcoded sets of targets
 #sets = [
 #  'agent': ['controller/src/agent:test', 'controller/src/cat:test'],
 #  'bgp': ['controller/src/bgp:test'],
 #  'opserver': ['src/contrail-analytics/contrail-collector:test'],
 #  'analytics': ['src/contrail-analytics/.*' and not opserver],
+#  'UNGROUPED': all excluding above
 #]
-
 # intersect set[TARGET_SET] with UNITTEST_TARGETS and store the result in $WORKSPACE/unittest_targets.lst
 
-# return 1 if set is empty
+if [[ "$TARGET_SET" == "agent" ]]; then
+  for target in 'controller/src/agent:test' 'controller/src/cat:test' ; do
+    if echo ",$UNITTEST_TARGETS," | grep -q ",$target," ; then
+      echo "$target" >> "$targets_file"
+    fi
+  done
+elif [[ "$TARGET_SET" == "bgp" ]]; then
+  if echo ",$UNITTEST_TARGETS," | grep -q ",controller/src/bgp:test," ; then
+    echo "controller/src/bgp:test" >> "$targets_file"
+  fi
+elif [[ "$TARGET_SET" == "opserver" ]]; then
+  if echo ",$UNITTEST_TARGETS," | grep -q ",src/contrail-analytics/contrail-collector:test," ; then
+    echo "src/contrail-analytics/contrail-collector:test" >> "$targets_file"
+  fi
+elif [[ "$TARGET_SET" == "analytics" ]]; then
+  for target in $(echo "$UNITTEST_TARGETS" | tr ',' ' ') ; do
+    if [[ "$target" =~ ^src/contrail-analytics/.*$ && "$target" != "src/contrail-analytics/contrail-opserver:test" ]] ; then
+      echo "$target" >> "$targets_file"
+    fi
+  done
+elif [[ "$TARGET_SET" == "UNGROUPED" ]]; then
+  excludes=',controller/src/agent:test,controller/src/cat:test,controller/src/bgp:test,'
+  for target in $(echo "$UNITTEST_TARGETS" | tr ',' ' ') ; do
+    if [[ ! "$target" =~ ^src/contrail-analytics/.*$ ]] && ! echo "$excludes" | grep -q ",$target," ; then
+      echo "$target" >> "$targets_file"
+    fi
+  done
+else
+  echo "ERROR: unknown target $TARGET_SET"
+  exit 1
+fi
+
+if [[ "$(cat $WORKSPACE/unittest_targets.lst | wc -l | xargs)" == '0' ]]; then
+  echo "INFO: filtered set is empty"
+  exit 1
+fi
+
+echo "INFO: filtered set:"
+cat $WORKSPACE/unittest_targets.lst
