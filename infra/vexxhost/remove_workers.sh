5f6b693b7c369e67d41c518817aa744bf54be233
tikitavi <rtikitavi@gmail.com>
2020-04-29 13:52:21 +0300

    Multinode support in infra (vexxhost, aws)
    Change-Id: Icad7db4762fa1c7a3ae532070fa5ca168b12057e

--- infra/vexxhost/remove_workers.sh
+++ infra/vexxhost/remove_workers.sh
@@ -12,7 +12,13 @@ source "$my_dir/definitions"
 source "$my_dir/functions.sh"
 source "$WORKSPACE/global.env"
 
-if nova show "$instance_id" | grep 'locked' | grep 'False'; then
-  down_instances $instance_id
-  nova delete "$instance_id"
-fi
+
+job_tag="JobTag=${BUILD_TAG}"
+instance_ids="$( list_instances ${job_tag} )"
+
+for instance_id in $instance_ids ; do
+  if nova show "$instance_id" | grep 'locked' | grep 'False'; then
+    down_instances $instance_id
+    nova delete "$instance_id"
+  fi
+done
