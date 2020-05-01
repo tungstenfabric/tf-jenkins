f585f823ae38770f498c80863489a5ec1fc8542d
Andrey Pavlov <andrey-mp@yandex.ru>
2020-05-01 10:23:13 +0300

    implement build-new

--- jobs/build/run-new.sh
+++ jobs/build/run-new.sh
@@ -14,11 +14,18 @@ if [ -e $WORKSPACE/patchsets-info.json ]; then
   cp -f $WORKSPACE/patchsets-info.json $WORKSPACE/src/tungstenfabric/tf-dev-env/input/
 fi
 
-rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS" $WORKSPACE/src $IMAGE_SSH_USER@$instance_ip:./
+ssh_cmd="ssh -i $WORKER_SSH_KEY $SSH_OPTIONS"
+rsync -a -e "$ssh_cmd" $WORKSPACE/src $IMAGE_SSH_USER@$instance_ip:./
 
 linux_distr=${TARGET_LINUX_DISTR["${ENVIRONMENT_OS}"]}
 
 echo "INFO: Build started"
+
+export DEVENV_TAG=${DEVENV_TAG:-stable${TAG_SUFFIX}}
+if grep -q "tungstenfabric/tf-dev-env" ./patchsets-info.json ; then
+  # changes in tf-dev-env - we have to rebuild it
+  export DEVENV_TAG="sandbox-$CONTRAIL_CONTAINER_TAG$TAG_SUFFIX"
+fi
 
 # build queens for test container always and add OPENSTACK_VERSION if it's different
 openstack_versions='queens'
@@ -26,34 +33,34 @@ if [[ "$OPENSTACK_VERSION" != 'queens' ]]; then
   openstack_versions+=",$OPENSTACK_VERSION"
 fi
 
-exit 0
-
 res=0
-cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $IMAGE_SSH_USER@$instance_ip || res=1
+cat <<EOF | $ssh_cmd $IMAGE_SSH_USER@$instance_ip || res=1
 [ "${DEBUG,,}" == "true" ] && set -x
-export WORKSPACE=\$HOME
 export DEBUG=$DEBUG
 export PATH=\$PATH:/usr/sbin
 
+export WORKSPACE=\$HOME
 # dont setup own registry
 export CONTRAIL_DEPLOY_REGISTRY=0
-
-export CONTAINER_REGISTRY=$CONTAINER_REGISTRY
-export SITE_MIRROR=$SITE_MIRROR
-
-export OPENSTACK_VERSIONS=$openstack_versions
-export CONTRAIL_CONTAINER_TAG=$CONTRAIL_CONTAINER_TAG$TAG_SUFFIX
-
 # to not to bind contrail sources to container
 export CONTRAIL_DIR=""
 
-export DEVENV_IMAGE_NAME=$DEVENV_IMAGE_NAME
-# devenftag is passed from parent fetch-sources job
+export LINUX_DISTR=$linux_distr
+export SITE_MIRROR=$SITE_MIRROR
+export GERRIT_URL=${GERRIT_URL}
+export GERRIT_BRANCH=${GERRIT_BRANCH}
+
+# devenftag is passed from parent job
 export DEVENV_TAG=$DEVENV_TAG
+export CONTAINER_REGISTRY=$CONTAINER_REGISTRY
+export CONTRAIL_CONTAINER_TAG=$CONTRAIL_CONTAINER_TAG$TAG_SUFFIX
+export OPENSTACK_VERSIONS=$openstack_versions
+export CONTRAIL_BUILD_FROM_SOURCE=${CONTRAIL_BUILD_FROM_SOURCE}
 export CONTRAIL_KEEP_LOG_FILES=true
 
-export LINUX_DISTR=$linux_distr
-export CONTRAIL_BUILD_FROM_SOURCE=${CONTRAIL_BUILD_FROM_SOURCE}
+# for the first stage prepare-stable
+export BUILD_DEV_ENV=1
+export BUILD_DEV_ENV_ON_PULL_FAIL=0
 
 cd src/tungstenfabric/tf-dev-env
 
@@ -89,11 +96,43 @@ case "${ENVIRONMENT_OS}" in
     ;;
 esac
 
-./run.sh build
+./run.sh "$STAGE" "$TARGET"
 EOF
 
 if [[ "$res" != '0' ]] ; then
-  echo "ERROR: Build failed"
+  echo "ERROR: Run failed. Stage: $STAGE  Target: $TARGET"
   exit $res
 fi
+
+rm -rf build.env
+touch build.env
+if [[ -z "$STAGE" ]]; then
+  # default stage meams sync sources. after sync we have to copy this file to publish it for UT
+  rsync -a -e "$ssh_cmd" $IMAGE_SSH_USER@$instance_ip:output/unittest_targets.lst $WORKSPACE/unittest_targets.lst || res=1
+  echo "export UNITTEST_TARGETS=$(cat $WORKSPACE/unittest_targets.lst | tr '\n' ',')" >> fetch.env
+fi
+
+if [[ -n "$PUBLISH" ]]; then
+  tag="$PUBLISH$TAG_SUFFIX"
+  if [[ "$tag" == 'build' ]];
+    tag="$CONTRAIL_CONTAINER_TAG$TAG_SUFFIX"
+  fi
+  cat <<EOF | $ssh_cmd $IMAGE_SSH_USER@$instance_ip || res=1
+set -eo pipefail
+export WORKSPACE=\$HOME
+export DEVENV_PUSH_TAG=$tag
+export CONTAINER_REGISTRY=$CONTAINER_REGISTRY
+src/tungstenfabric/tf-dev-env/run.sh upload
+EOF
+
+  if [[ "$res" != '0' ]] ; then
+    echo "ERROR: Publish failed for tag $tag. Stage: $STAGE  Target: $TARGET"
+    exit $res
+  fi
+
+  # save DEVENV_TAG that was pushed by this job
+  # chidlren jobs may have own TAG_SUFFIX and they shouldn't rely on it
+  echo "export DEVENV_TAG=$tag" > build.env
+fi
+
 echo "INFO: Build finished successfully"
