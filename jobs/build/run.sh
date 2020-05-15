f04f7c79a7e4abbc710f8bb7dccc6a4cacad7143
Andrey Pavlov <andrey-mp@yandex.ru>
2020-05-15 12:04:48 +0300

    fix typo PIPLINE -> PIPELINE

--- jobs/build/run.sh
+++ jobs/build/run.sh
@@ -8,8 +8,8 @@ my_dir="$(dirname $my_file)"
 
 source "$my_dir/definitions"
 
-if [[ -n "$PIPLINE_FILTER" && ! ",$PIPLINE_FILTER," =~ ",$GERRIT_PIPELINE," ]]; then
-  echo "INFO: Pipeline filter is not empty ($PIPLINE_FILTER) and doesn't contain current pipeline '$GERRIT_PIPELINE'"
+if [[ -n "$PIPELINE_FILTER" && ! ",$PIPELINE_FILTER," =~ ",$GERRIT_PIPELINE," ]]; then
+  echo "INFO: Pipeline filter is not empty ($PIPELINE_FILTER) and doesn't contain current pipeline '$GERRIT_PIPELINE'"
   exit
 fi
 
