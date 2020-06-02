ddc3ad6ad67d7ae56b6f968bc925afb9fe696a65
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-06-02 18:22:51 +0300

    Add ability to deploy rhosp on bare metal
    Partial-bug: CEM-14928
    Change-Id: If61982350e4ab5dc9b256b020f5343c634319973

--- jobs/devstack/rhosp/create_workers.sh
+++ jobs/devstack/rhosp/create_workers.sh
@@ -6,31 +6,6 @@ set -o pipefail
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
-source "$WORKSPACE/global.env"
-source "$my_dir/../../../infra/${SLAVE}/definitions"
 source "$my_dir/definitions"
 
-stackrc_file=${stackrc_file:-"stackrc.$JOB_NAME.env"}
-stackrc_file_path=$WORKSPACE/$stackrc_file
-
-echo "export OS_REGION_NAME=${OS_REGION_NAME}" > "$stackrc_file_path"
-IMAGE_SSH_USER=${OS_IMAGE_USERS["${ENVIRONMENT_OS^^}"]}
-echo "export SSH_USER=$IMAGE_SSH_USER" >> "$stackrc_file_path"
-echo "export IMAGE_SSH_USER=$IMAGE_SSH_USER" >> "$stackrc_file_path"
-echo "export DEPLOY_COMPACT_AIO=$DEPLOY_COMPACT_AIO" >> "$stackrc_file_path"
-echo "export ENABLE_RHEL_REGISTRATION=$ENABLE_RHEL_REGISTRATION" >> "$stackrc_file_path"
-
-# wait for free resource
-while true; do
-  [[ "$(($(nova list --tags "SLAVE=$SLAVE"  --field status | grep -c 'ID\|ACTIVE') - 1))" -lt "$MAX_COUNT_VM" ]] && break
-  sleep 60
-done
-
-while true; do
-  [[ "$(nova quota-show --detail | grep cores | sed 's/}.*/}/'| tr -d "}" | awk '{print $NF}')" -lt "$MAX_COUNT_VCPU" ]] && break
-  sleep 60
-done
-
-
-cd src/tungstenfabric/tf-devstack/rhosp
-vexxrc="$stackrc_file_path" ./providers/vexx/create_env.sh
+$my_dir/../../../infra/${CLOUD}/create_worker.sh
