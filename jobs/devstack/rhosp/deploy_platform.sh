29f493ed9b552f9feda79cf3e82d38f8aa314393
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-06-02 18:22:51 +0300

    Add ability to deploy rhosp on bare metal
    Partial-bug: CEM-14928
    Change-Id: If61982350e4ab5dc9b256b020f5343c634319973

--- jobs/devstack/rhosp/deploy_platform.sh
+++ jobs/devstack/rhosp/deploy_platform.sh
@@ -11,26 +11,12 @@ source "$my_dir/definitions"
 stackrc_file=${stackrc_file:-"stackrc.$JOB_NAME.env"}
 stackrc_file_path=$WORKSPACE/$stackrc_file
 
+function add_deployrc() {
+  local file="$1"
+  cat <<EOF >> "$file"
 source $stackrc_file_path
-
-echo "INFO: Deploy platform for $JOB_NAME"
-rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS" $stackrc_file_path $IMAGE_SSH_USER@$mgmt_ip:./
-rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS" $WORKSPACE/src $IMAGE_SSH_USER@$mgmt_ip:./
-
-cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $IMAGE_SSH_USER@$mgmt_ip || res=1
-export RHEL_USER=$RHEL_USER
-export RHEL_PASSWORD=$RHEL_PASSWORD
-[ "${DEBUG,,}" == "true" ] && set -x
-export WORKSPACE=\$HOME
-export DEBUG=$DEBUG
-export CONTAINER_REGISTRY="$CONTAINER_REGISTRY"
-export CONTRAIL_CONTAINER_TAG="$CONTRAIL_CONTAINER_TAG$TAG_SUFFIX"
-
-export PATH=\$PATH:/usr/sbin
-source $stackrc_file
-cd src/tungstenfabric/tf-devstack/rhosp
-./run.sh platform
 EOF
+}
+export -f add_deployrc
 
-echo "INFO: Deploy platform finished"
-exit $res
+${my_dir}/../common/deploy_platform.sh rhosp
