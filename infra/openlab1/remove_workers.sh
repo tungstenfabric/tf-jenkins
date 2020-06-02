1a355d8901710691d5f9d21dfe122a69e43aee16
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-06-02 18:22:51 +0300

    Add ability to deploy rhosp on bare metal
    Partial-bug: CEM-14928
    Change-Id: If61982350e4ab5dc9b256b020f5343c634319973

--- infra/openlab1/remove_workers.sh
+++ infra/openlab1/remove_workers.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -eE
 set -o pipefail
 
 [ "${DEBUG,,}" == "true" ] && set -x
@@ -7,5 +7,3 @@ my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
 source "$my_dir/definitions"
-
-${my_dir}/../common/collect_logs.sh rhosp
