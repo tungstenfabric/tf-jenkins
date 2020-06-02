be591f4e9a79a0e7e780de899898f470e371de26
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-06-02 18:22:51 +0300

    Add ability to deploy rhosp on bare metal
    Partial-bug: CEM-14928
    Change-Id: If61982350e4ab5dc9b256b020f5343c634319973

--- jobs/devstack/rhosp/deploy_tf.sh
+++ jobs/devstack/rhosp/deploy_tf.sh
@@ -6,31 +6,17 @@ set -o pipefail
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
+
 source "$my_dir/definitions"
 # stackrc file is prepared by pipeline based on 
 # previous job's artefacts
-stackrc_file=${stackrc_file:-"deps.${JOB_NAME}.${JOB_RND}.env"}
-stackrc_file_path=$WORKSPACE/$stackrc_file
+export stackrc_file=${stackrc_file:-"deps.${JOB_NAME}.${JOB_RND}.env"}
+export stackrc_file_path=$WORKSPACE/$stackrc_file
 
-source $stackrc_file_path
+function add_deployrc() {
+  local file="$1"
+  cat "$stackrc_file_path" >> "$file"
+}
+export -f add_deployrc
 
-echo 'INFO: Deploy RHOSP overcloud'
-rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS" $stackrc_file_path $IMAGE_SSH_USER@$mgmt_ip:./
-rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS" $WORKSPACE/src $IMAGE_SSH_USER@$mgmt_ip:./
-
-cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $IMAGE_SSH_USER@$mgmt_ip || res=1
-export RHEL_USER=$RHEL_USER
-export RHEL_PASSWORD=$RHEL_PASSWORD
-[ "${DEBUG,,}" == "true" ] && set -x
-export WORKSPACE=\$HOME
-export DEBUG=$DEBUG
-export CONTAINER_REGISTRY="$CONTAINER_REGISTRY"
-export CONTRAIL_CONTAINER_TAG="$CONTRAIL_CONTAINER_TAG$TAG_SUFFIX"
-export PATH=\$PATH:/usr/sbin
-source $stackrc_file
-cd src/tungstenfabric/tf-devstack/rhosp
-./run.sh
-EOF
-
-echo "INFO: Deploy TF finished"
-exit $res
+${my_dir}/../common/deploy_tf.sh rhosp
