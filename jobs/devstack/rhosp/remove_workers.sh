a2434f0a3628d40e412910aa28ce96b25fa962e3
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-06-02 18:22:51 +0300

    Add ability to deploy rhosp on bare metal
    Partial-bug: CEM-14928
    Change-Id: If61982350e4ab5dc9b256b020f5343c634319973

--- jobs/devstack/rhosp/remove_workers.sh
+++ jobs/devstack/rhosp/remove_workers.sh
@@ -10,5 +10,7 @@ source "$my_dir/definitions"
 stackrc_file=${stackrc_file:-"deps.${JOB_NAME}.${JOB_RND}.env"}
 stackrc_file_path=$WORKSPACE/$stackrc_file
 
+[[ "$CLOUD" == 'bmc' ]] && exit
+
 cd src/tungstenfabric/tf-devstack/rhosp/providers/vexx
 vexxrc="$stackrc_file_path" ./cleanup.sh
