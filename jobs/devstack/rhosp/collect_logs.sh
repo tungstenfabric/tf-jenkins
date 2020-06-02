4b2e89d2a4046840ad2ebacd9826a22fba90a0ce
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-06-02 18:22:51 +0300

    Add ability to deploy rhosp on bare metal
    Partial-bug: CEM-14928
    Change-Id: If61982350e4ab5dc9b256b020f5343c634319973

--- jobs/devstack/rhosp/collect_logs.sh
+++ jobs/devstack/rhosp/collect_logs.sh
@@ -8,23 +8,4 @@ my_dir="$(dirname $my_file)"
 
 source "$my_dir/definitions"
 
-cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $IMAGE_SSH_USER@$mgmt_ip || res=1
-[ "${DEBUG,,}" == "true" ] && set -x
-export WORKSPACE=\$HOME
-export DEBUG=$DEBUG
-export PATH=\$PATH:/usr/sbin
-cd tf-devstack/rhosp
-./run.sh logs
-EOF
-
-rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS" $IMAGE_SSH_USER@$mgmt_ip:logs.tgz $WORKSPACE/logs.tgz
-
-pushd $WORKSPACE
-tar -xzf logs.tgz
-ssh -i $LOGS_HOST_SSH_KEY $SSH_OPTIONS $LOGS_HOST_USERNAME@$LOGS_HOST "mkdir -p $FULL_LOGS_PATH"
-rsync -a -e "ssh -i $LOGS_HOST_SSH_KEY $SSH_OPTIONS" $WORKSPACE/logs $LOGS_HOST_USERNAME@$LOGS_HOST:$FULL_LOGS_PATH
-rm -rf $WORKSPACE/logs
-echo "INFO: Logs collected at ${LOGS_URL}/${JOB_LOGS_PATH}"
-popd
-
-exit $res
+${my_dir}/../common/collect_logs.sh rhosp
