9aa5b8a40691c5c0e6598ed67f86fc3b333ffb74
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-06-05 13:50:56 +0300

    Rework deploy scripts maaas
    Change-Id: I79c90887c67dbbb83c1e1827047d25a4c80102aa

--- jobs/devstack/juju/create_workers.sh
+++ jobs/devstack/juju/create_workers.sh
@@ -13,7 +13,25 @@ if [[ "$CLOUD" == 'maas' ]] ; then
     echo "ERROR: current maas cloud works only for vexxhost slave"
     exit 1
   fi
-  "$my_dir/create_workers_openlab.sh"
+  "$my_dir/../../../infra/openlab2/create_workers.sh"
+  ENV_FILE="$WORKSPACE/stackrc.$JOB_NAME.env"
+  source "$ENV_FILE"
+  ssh_cmd="ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS"
+  rsync -a -e "$ssh_cmd" $WORKSPACE/src $IMAGE_SSH_USER@$instance_ip:./
+  cat <<EOF >> $WORKSPACE/run_deploy_maas.sh
+  cat <<EOF > run_deploy_maas.sh
+#!/bin/bash -e
+[ "${DEBUG,,}" == "true" ] && set -x
+export IPMI_IPS='$IPMI_IPS'
+cd \$HOME/src/tungstenfabric/tf-devstack/common
+./deploy_maas.sh \$HOME/maas.vars
+EOF
+  chmod a+x $WORKSPACE/run_deploy_maas.sh
+
+  rsync -a -e "$ssh_cmd" $WORKSPACE/run_deploy_maas.sh $IMAGE_SSH_USER@$instance_ip:./
+  # run this via eval due to special symbols in ssh_cmd
+  eval $ssh_cmd $IMAGE_SSH_USER@$instance_ip ./run_deploy_maas.sh
+
 else
   "$my_dir/../common/create_workers.sh"
 fi
