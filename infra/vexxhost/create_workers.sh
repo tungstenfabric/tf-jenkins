8e627addb58765ab44e9a93836e378ffcef092c0
tikitavi <rtikitavi@gmail.com>
2020-06-08 22:41:56 +0300

    check the results of nova boot
    Change-Id: I41dba83220f522969d6b9daa54980ce24fbef1b6

--- infra/vexxhost/create_workers.sh
+++ infra/vexxhost/create_workers.sh
@@ -97,6 +97,7 @@ for (( i=1; i<=$VM_RETRIES ; ++i )) ; do
      sleep 60
    done
 
+  res=0
   nova boot --flavor ${INSTANCE_TYPE} \
             --security-groups ${OS_SG} \
             --key-name=worker \
@@ -105,9 +106,9 @@ for (( i=1; i<=$VM_RETRIES ; ++i )) ; do
             --nic net-name=${OS_NETWORK} \
             --block-device source=image,id=$IMAGE,dest=volume,shutdown=remove,size=120,bootindex=0 \
             --poll \
-            ${instance_name}
+            ${instance_name} || res=1
 
-  if [[ $? != 0 ]] ; then
+  if [[ $res == 1 ]]; then
     echo "ERROR: Instances creation is failed on nova boot. Retry"
     cleanup ${group_tag}
     sleep 60
