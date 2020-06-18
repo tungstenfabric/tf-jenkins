668e4512e83afac4aa6e8e21434e5c09ec764696
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-06-18 12:55:05 +0300

    Enable rhosp deploy on bare metal for nightly job
    Depends-On: I4e4fbcd64de6b7346d8b817408af6a8abb7311e3
    partial-jira-bug: CEM-14928
    Change-Id: If06a41edc3483cf762dc448a4aaa004a08e1bd31

--- jobs/devstack/rhosp/remove_workers.sh
+++ jobs/devstack/rhosp/remove_workers.sh
@@ -10,5 +10,9 @@ source "$my_dir/definitions"
 stackrc_file=${stackrc_file:-"deps.${JOB_NAME}.${JOB_RND}.env"}
 stackrc_file_path=$WORKSPACE/$stackrc_file
 
+if [[ "$PROVIDER" == "bmc" ]]; then
+    exit
+fi
+
 cd src/tungstenfabric/tf-devstack/rhosp/providers/vexx
 vexxrc="$stackrc_file_path" ./cleanup.sh
