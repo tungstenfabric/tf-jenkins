19c715b843fe8d0a887c9c118c646288f00d1bba
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-06-18 12:55:05 +0300

    Enable rhosp deploy on bare metal for nightly job
    Depends-On: I4e4fbcd64de6b7346d8b817408af6a8abb7311e3
    partial-jira-bug: CEM-14928
    Change-Id: If06a41edc3483cf762dc448a4aaa004a08e1bd31

--- infra/openlab1/create_workers.sh
+++ infra/openlab1/create_workers.sh
@@ -11,6 +11,7 @@ source "$my_dir/definitions"
 
 cat <<EOF | ssh -i $OPENLAB1_SSH_KEY $SSH_OPTIONS -p 30001 jenkins@openlab.tf-jenkins.progmaticlab.com
 [ "${DEBUG,,}" == "true" ] && set -x
+export LIBVIRT_DEFAULT_URI=qemu:///system
 export PATH=\$PATH:/usr/sbin
 virsh destroy $VM_NAME || /bin/true
 virsh undefine $VM_NAME  || /bin/true
@@ -22,7 +23,7 @@ EOF
 
 stackrc_file=${stackrc_file:-"stackrc.$JOB_NAME.env"}
 stackrc_file_path=$WORKSPACE/$stackrc_file
-echo "export IMAGE_SSH_USER=stack" > "$stackrc_file"
+echo "export IMAGE_SSH_USER=$IMAGE_SSH_USER" > "$stackrc_file"
 echo "export instance_ip=$INSTANCE_IP" >> "$stackrc_file"
 echo "export SSH_EXTRA_OPTIONS=\"-o ProxyCommand=\\\"ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -W %h:%p -i \$OPENLAB1_SSH_KEY -l jenkins -p 30001 openlab.tf-jenkins.progmaticlab.com\\\"\"" >> "$stackrc_file"
 echo "export mgmt_ip=$INSTANCE_IP" >> "$stackrc_file"
@@ -30,7 +31,7 @@ source "$stackrc_file"
 
 timeout 300 bash -c "\
 while /bin/true ; do \
-  ssh -i $OPENLAB1_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS $IMAGE_SSH_USER@$instance_ip 'uname -a' && break ; \
+  ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS $IMAGE_SSH_USER@$instance_ip 'uname -a' && break ; \
   sleep 10 ; \
 done"
 
