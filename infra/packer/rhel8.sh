64483e33276f475b8d3254befe731f5ca3e5958b
Evgeny Shapovalov <eshapovalov.dev@gmail.com>
2020-06-22 15:01:54 +0300

    Packer script for rhel8
    Description:
    Add repo clean up
    Disable subscription-manager
    Add gpg-key check
    
    Change-Id: I21012ff6aafaedeae287058cdf7ef826484ec505

--- infra/packer/rhel8.sh
+++ infra/packer/rhel8.sh
@@ -8,15 +8,25 @@ Name=Red Hat Enterprise Linux 8.0 BaseOS
 enabled=1
 gpgcheck=1
 baseurl=http://rhel8-mirrors.tf-jenkins.progmaticlab.com/base/rhel-8-for-x86_64-baseos-rpms/
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
 
 [AppStream]
 Name=Red Hat Enterprise Linux 8.0 AppStream
 enabled=1
 gpgcheck=1
 baseurl=http://rhel8-mirrors.tf-jenkins.progmaticlab.com/appstream/rhel-8-for-x86_64-appstream-rpms/
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release
 
 EOF
 
+sudo mv /etc/yum.repos.d/redhat.repo ~/
+sudo yum clean all
+sudo subscription-manager clean
+
+#prevent subscription manager error
+sudo sed 's/enabled=1/enabled=0/g' /etc/yum/pluginconf.d/subscription-manager.conf > subscription-manager.conf.temp
+sudo cp -f subscription-manager.conf.temp /etc/yum/pluginconf.d/subscription-manager.conf
+rm -rf subscription-manager.conf.temp
 sudo mv local.repo /etc/yum.repos.d/
 
 sudo yum update -y
