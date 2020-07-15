b29a2ffd0e482fabcbc2f3bd4f777c64a4ac2f17
Arina Altynova <aaltynova@progmaticlab.com>
2020-07-15 11:45:33 +0300

    ziu. part 1
    Change-Id: I0db0c53223a4ca44105d98ee4039839d4b8dfa2b

---
+++ jobs/test/ziu/run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -eE
+set -o pipefail
+
+[ "${DEBUG,,}" == "true" ] && set -x
+
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname $my_file)"
+
+source "$my_dir/definitions"
+
+echo "INFO: Test ziu started"
+
+cat << EOF > $WORKSPACE/test-ziu.sh
+#!/bin/bash -e
+[ "${DEBUG,,}" == "true" ] && set -x
+export WORKSPACE=\$HOME
+export DEBUG=$DEBUG
+export ORCHESTRATOR=$ORCHESTRATOR
+export CONTAINER_REGISTRY="$CONTAINER_REGISTRY"
+export CONTRAIL_CONTAINER_TAG="$CONTRAIL_CONTAINER_TAG$TAG_SUFFIX"
+export SSL_ENABLE=$SSL_ENABLE
+export PATH=\$PATH:/usr/sbin
+src/tungstenfabric/tf-dev-test/ziu-test/run.sh
+EOF
+chmod a+x $WORKSPACE/test-ziu.sh
+
+ssh_cmd="ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS"
+rsync -a -e "$ssh_cmd" {$WORKSPACE/src,$WORKSPACE/test-ziu.sh} $IMAGE_SSH_USER@$instance_ip:./
+# run this via eval due to special symbols in ssh_cmd
+eval $ssh_cmd $IMAGE_SSH_USER@$instance_ip ./test-ziu.sh || res=1
+
+echo "INFO: Test ziu finished"
+exit $res
