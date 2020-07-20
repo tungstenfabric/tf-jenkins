9a7f63fa373f779c07bb147c6ffd7be1f685528b
Arina Altynova <aaltynova@progmaticlab.com>
2020-07-21 02:12:25 +0300

    ziu test extra logs [tf-jenkins]
    Change-Id: I34e34550a2ece312680919b0b3a727d57178e727

--- jobs/test/ziu/run.sh
+++ jobs/test/ziu/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eE
 set -o pipefail
-
+set -x
 [ "${DEBUG,,}" == "true" ] && set -x
 
 my_file="$(readlink -e "$0")"
@@ -9,6 +9,13 @@ my_dir="$(dirname $my_file)"
 source "$my_dir/definitions"
 
 echo "INFO: Test ziu started"
+
+ENV_FILE="$WORKSPACE/stackrc.$JOB_NAME.env"
+source $ENV_FILE
+
+echo "##########  $ENV_FILE  ##########"
+cat $ENV_FILE
+echo "#################################"
 
 cat << EOF > $WORKSPACE/test-ziu.sh
 #!/bin/bash -e
@@ -23,10 +30,18 @@ export PATH=\$PATH:/usr/sbin
 src/tungstenfabric/tf-dev-test/ziu-test/run.sh
 EOF
 chmod a+x $WORKSPACE/test-ziu.sh
-
+echo "##########  $WORKSPACE/test-ziu.sh  ##########"
+cat $WORKSPACE/test-ziu.sh
+echo "##############################################"
 ssh_cmd="ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS"
+echo "[test/zui/run.sh]     WORKER_SSH_KEY=$WORKER_SSH_KEY"
+echo "[test/zui/run.sh]        SSH_OPTIONS=$SSH_OPTIONS"
+echo "[test/zui/run.sh]  SSH_EXTRA_OPTIONS=$SSH_EXTRA_OPTIONS"
+echo "[test/zui/run.sh]     IMAGE_SSH_USER=$IMAGE_SSH_USER"
+echo "[test/zui/run.sh]        instance_ip=$instance_ip"
 rsync -a -e "$ssh_cmd" {$WORKSPACE/src,$WORKSPACE/test-ziu.sh} $IMAGE_SSH_USER@$instance_ip:./
 # run this via eval due to special symbols in ssh_cmd
+echo "[test/ziu/run.sh]  eval $ssh_cmd $IMAGE_SSH_USER@$instance_ip ./test-ziu.sh || res=1"
 eval $ssh_cmd $IMAGE_SSH_USER@$instance_ip ./test-ziu.sh || res=1
 
 echo "INFO: Test ziu finished"
