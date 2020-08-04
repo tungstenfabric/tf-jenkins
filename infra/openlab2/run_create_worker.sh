fd9c362f8ddf3ed28a964ba9354649bacf8a674a
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-08-04 17:05:38 +0300

    test_maas
    Change-Id: I8268ffc58bd717e1d70e748a4802c714b4a814ba

---
+++ infra/openlab2/run_create_worker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -eE
+set -o pipefail
+
+[ "${DEBUG,,}" == "true" ] && set -x
+
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname $my_file)"
+
+source "$my_dir/definitions"
+source "$my_dir/functions.sh"
+export LIBVIRT_DEFAULT_URI=qemu:///system
+spin_vm MAAS_CONTROLLER
