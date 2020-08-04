913ef0986059b71f9ff3e2ecec561412f0c389f0
Evgeny Kuznetsov <evgeny.kuznetsov@outlook.com>
2020-08-04 17:05:38 +0300

    test_maas
    Change-Id: I8268ffc58bd717e1d70e748a4802c714b4a814ba

--- jobs/devstack/juju/create_workers.sh
+++ jobs/devstack/juju/create_workers.sh
@@ -18,19 +18,23 @@ if [[ "$CLOUD" == 'maas' ]] ; then
   source "$ENV_FILE"
   ssh_cmd="ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS"
   rsync -a -e "$ssh_cmd" $WORKSPACE/src $IMAGE_SSH_USER@$instance_ip:./
-  cat <<EOF >> $WORKSPACE/run_deploy_maas.sh
+  cat <<EOF >> $WORKSPACE/prepare_maas.sh
 #!/bin/bash -e
 [ "${DEBUG,,}" == "true" ] && set -x
-export IPMI_IPS='$IPMI_IPS'
-cd \$HOME/src/tungstenfabric/tf-devstack/common
-./deploy_maas.sh \$HOME/maas.vars
+export DEBIAN_FRONTEND=noninteractive
+export MAAS_API_KEY=$MAAS_API_KEY
+export PROFILE=admin
+export MAAS_ENDPOINT="http://192.168.51.5:5240/MAAS"
+sudo apt-get install maas-cli -y
+maas login $PROFILE $MAAS_ENDPOINT - <<< $(echo $MAAS_API_KEY)
+maas $PROFILE nodes read
 EOF
-  chmod a+x $WORKSPACE/run_deploy_maas.sh
+  chmod a+x $WORKSPACE/prepare_maas.sh
 
-  rsync -a -e "$ssh_cmd" $WORKSPACE/run_deploy_maas.sh $IMAGE_SSH_USER@$instance_ip:./
+  rsync -a -e "$ssh_cmd" $WORKSPACE/prepare_maas.sh $IMAGE_SSH_USER@$instance_ip:./
   # run this via eval due to special symbols in ssh_cmd
-  eval $ssh_cmd $IMAGE_SSH_USER@$instance_ip ./run_deploy_maas.sh
-
+  eval $ssh_cmd $IMAGE_SSH_USER@$instance_ip ./prepare_maas.sh
+  
 else
   "$my_dir/../common/create_workers.sh"
 fi
