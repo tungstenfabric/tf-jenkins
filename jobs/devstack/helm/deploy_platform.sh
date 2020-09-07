468d5a3e1ad460326ae5c0e446a5ae9fea011e27
Klimkov Dmitriy <dmklimkov@gmail.com>
2020-09-07 23:28:25 +0300

    Set locale
    Change-Id: I60445a492a53cc2d372d6125130a06762deb30a6

--- jobs/devstack/helm/deploy_platform.sh
+++ jobs/devstack/helm/deploy_platform.sh
@@ -8,4 +8,19 @@ my_dir="$(dirname $my_file)"
 
 source "$my_dir/definitions"
 
+function set_locale() {
+  sudo localectl set-locale LANG=en-US.UTF-8
+  . /etc/locale.conf
+  export LC_ALL=en_US.UTF-8
+  cat /etc/environment
+  # If /etc/environment already contain LANG, LC_ALL we need replace it by "sudo sed ..."
+  if ! (grep "LANG" /etc/environment); then
+    if ! (grep "LC_ALL" /etc/environment); then
+      sudo sh -c "echo -e 'LANG=en_US.utf-8\nLC_ALL=en_US.utf-8' >> /etc/environment"
+      cat /etc/environment
+    fi
+  fi
+}
+export -f set_locale
+
 ${my_dir}/../common/deploy_platform.sh helm
