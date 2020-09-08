cf705ca112f3cfcae5bb9736f97d69f5dedd4473
Klimkov Dmitriy <dmklimkov@gmail.com>
2020-09-08 13:36:55 +0300

    Set locale for helm
    Helm installation requires set locale, otherwise get UnicodeDecodeError on Vexx
    
    Change-Id: I60445a492a53cc2d372d6125130a06762deb30a6

--- jobs/devstack/helm/deploy_platform.sh
+++ jobs/devstack/helm/deploy_platform.sh
@@ -8,4 +8,18 @@ my_dir="$(dirname $my_file)"
 
 source "$my_dir/definitions"
 
+function add_deployrc() {
+  local file="$1"
+  cat <<EOF >> "$file"
+  # Openstack Helm installation requires set locale, otherwise get UnicodeDecodeError on Vexx
+  sudo localectl set-locale LANG=en-US.UTF-8
+  . /etc/locale.conf
+  export LC_ALL=en_US.UTF-8
+  if ! grep -q "LANG" /etc/environment && ! grep -q "LC_ALL" /etc/environment ; then
+    sudo bash -c "echo -e 'LANG=en_US.utf-8\nLC_ALL=en_US.utf-8' >> /etc/environment"
+  fi
+EOF
+}
+export -f add_deployrc
+
 ${my_dir}/../common/deploy_platform.sh helm
