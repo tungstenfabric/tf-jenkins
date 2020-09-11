6b0a5033e8c128efcbe3b9cd3662677e3f96442e
Andrey Pavlov <andrey-mp@yandex.ru>
2020-09-11 21:06:05 +0300

    fix previous fix with disabling pip mirror
    Change-Id: Iaf437be60bee6e8e3a556541d5cdca29b174ae1a

--- infra/hooks/ubuntu/up.sh
+++ infra/hooks/ubuntu/up.sh
@@ -16,7 +16,7 @@ source "${WORKSPACE}/vars.${JOB_NAME}.${JOB_RND}.env" || /bin/true
 #rsync -a -e "ssh -i ${WORKER_SSH_KEY} ${SSH_OPTIONS}" "$my_dir/../../mirrors/mirror-pip.conf" ${IMAGE_SSH_USER}@${instance_ip}:./pip.conf
 
 cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $IMAGE_SSH_USER@$instance_ip
-sudo cp -f ./pip.conf /etc/pip.conf
+#sudo cp -f ./pip.conf /etc/pip.conf
 echo "INFO: Update ubuntu OS"
 echo "APT::Acquire::Retries \"10\";" | sudo tee /etc/apt/apt.conf.d/80-retries
 sudo cp -f /usr/share/unattended-upgrades/20auto-upgrades-disabled /etc/apt/apt.conf.d/ || /bin/true
