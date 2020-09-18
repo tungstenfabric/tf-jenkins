d7391d54921dc12e3341ffa4ef439eeb8356eaae
alexey-mr <alexey.morlang@gmail.com>
2020-09-18 19:58:52 +0300

    fixed rhel8 mirror update script
    Change-Id: Ib20ed825433808ae31beb98d7202c83d68c67658

--- infra/mirrors/rhel8/update_repo.sh
+++ infra/mirrors/rhel8/update_repo.sh
@@ -1,14 +1,12 @@
 #!/bin/bash -ex
 
-mkdir -p /var/www/html/repos/{ansible,datapath,openstack,ha,satellite}
+
+mkdir -p /var/www/html/repos/{ansible,appstream,base,datapath,openstack,ha,satellite}
 
 reposync  -p /var/www/html/repos/ansible --download-metadata --repo=ansible-2-for-rhel-8-x86_64-rpms
-reposync  -p /var/www/html/repos/ansible --download-metadata  --repo=ansible-2.8-for-rhel-8-x86_64-rpms   
-
+reposync  -p /var/www/html/repos/appstream --download-metadata --repo=rhel-8-for-x86_64-appstream-rpms
+reposync  -p /var/www/html/repos/base --download-metadata --repo=rhel-8-for-x86_64-baseos-rpms
 reposync  -p /var/www/html/repos/datapath --download-metadata  --repo=fast-datapath-for-rhel-8-x86_64-rpms 
-
 reposync  -p /var/www/html/repos/openstack --download-metadata  --repo=openstack-16.1-for-rhel-8-x86_64-rpms
-reposync  -p /var/www/html/repos/openstack --download-metadata  --repo=openstack-16-for-rhel-8-x86_64-rpms  
-
 reposync  -p /var/www/html/repos/ha --download-metadata  --repo=rhel-8-for-x86_64-highavailability-rpms
 reposync  -p /var/www/html/repos/satellite --download-metadata  --repo=satellite-tools-6.5-for-rhel-8-x86_64-rpms
