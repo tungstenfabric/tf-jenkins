9750eecae078fbcac60898618f3cc43502a15b28
alexey-mr <alexey.morlang@gmail.com>
2020-10-15 16:15:16 +0300

    rhosp commonize prepare for bmc and vexx
    Depends-On: I25fe7c2dad5f3ec060736b7e351acf4294d23196
    Change-Id: I6be48608c258f27e97d9e64642d7d24c122a3d18

--- jobs/devstack/rhosp/create_workers.sh
+++ jobs/devstack/rhosp/create_workers.sh
@@ -39,8 +39,7 @@ else
       [[ "$(nova quota-show --detail | grep cores | sed 's/}.*/}/'| tr -d "}" | awk '{print $NF}')" -lt "$MAX_COUNT_VCPU" ]] && break
       sleep 60
     done
-
-
-    cd src/tungstenfabric/tf-devstack/rhosp
-    vexxrc="$stackrc_file_path" ./providers/vexx/create_env.sh
 fi
+
+# to prepare rhosp-provisionin.sh
+vexxrc="$stackrc_file_path" ./src/tungstenfabric/tf-devstack/rhosp/run.sh provisioning
