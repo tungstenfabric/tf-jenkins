6edca0900a1d8e4a6b98f06f3b543f4124441367
alexey-mr <alexey.morlang@gmail.com>
2020-10-29 14:48:19 +0300

    Added nopassword option for ssh opts
    - it is to avoid possible hangs on password ask
    
    Change-Id: I42f14382fb90c9f8be4cff70dec19c09e5eee251

--- jobs/devstack/rhosp/create_workers.sh
+++ jobs/devstack/rhosp/create_workers.sh
@@ -19,7 +19,7 @@ if [ -n "$TAG_SUFFIX" ] ; then
   export CONTRAIL_CONTAINER_TAG="$CONTRAIL_CONTAINER_TAG$TAG_SUFFIX"
   export CONTRAIL_DEPLOYER_CONTAINER_TAG="$CONTRAIL_DEPLOYER_CONTAINER_TAG$TAG_SUFFIX"
   export CONTRAIL_CONTAINER_TAG_ORIGINAL="$CONTRAIL_CONTAINER_TAG_ORIGINAL$TAG_SUFFIX"
-  export CONTRAIL_DEPLOYER_CONTAINER_TAG_ORIGINAL"$CONTRAIL_DEPLOYER_CONTAINER_TAG_ORIGINAL$TAG_SUFFIX"
+  export CONTRAIL_DEPLOYER_CONTAINER_TAG_ORIGINAL="$CONTRAIL_DEPLOYER_CONTAINER_TAG_ORIGINAL$TAG_SUFFIX"
 fi
 
 for (( i=1; i<=$VM_RETRIES ; ++i )) ; do
@@ -37,7 +37,6 @@ for (( i=1; i<=$VM_RETRIES ; ++i )) ; do
 
       echo "export OS_REGION_NAME=${OS_REGION_NAME}" >> "$stackrc_file_path"
       IMAGE_SSH_USER=${OS_IMAGE_USERS["${ENVIRONMENT_OS^^}"]}
-      echo "export SSH_USER=$IMAGE_SSH_USER" >> "$stackrc_file_path"
       echo "export IMAGE_SSH_USER=$IMAGE_SSH_USER" >> "$stackrc_file_path"
 
       # wait for free resource
@@ -51,8 +50,10 @@ for (( i=1; i<=$VM_RETRIES ; ++i )) ; do
         sleep 60
       done
   fi
+  echo "export SSH_USER=$IMAGE_SSH_USER" >> "$stackrc_file_path"
 
   # to prepare rhosp-provisionin.sh
+  source $stackrc_file_path
   if ./src/tungstenfabric/tf-devstack/rhosp/create_env.sh ; then
     exit 0
   fi
