8719a2652bef45be0a05e944e41ab13a1f09eb11
Sergey Baukin <baukin@gmail.com>
2020-11-10 13:21:41 +0300

    WIP: log patchset url
    Change-Id: I45b5226520c2eaa2e5a26d9b4e4f5416ba70c0d7

--- infra/fluentd/log.py
+++ infra/fluentd/log.py
@@ -15,6 +15,8 @@ tags = [
 results = [
     'status',
     'duration',
+    'started',
+    'patchset',
     'logs',
 ]
 
@@ -35,7 +37,7 @@ def countprevious(days, measurement, logitem):
     points = list(res.get_points())
     successcount = len([p for p in points if p['status'] == "SUCCESS"])
     if logitem['status'] == "SUCCESS":
-        successcount += 1 
+        successcount += 1
     return "{}/{}".format(successcount, len(points)+1)
 
 def splitdata(logdata):
