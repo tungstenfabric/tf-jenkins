081c14a2f9b5dfe333302c1ba42f9314a66eff29
Andrey Pavlov <andrey-mp@yandex.ru>
2020-11-16 17:49:18 +0300

    try to avoid cert issue on review.opencontrail.org
    Change-Id: Id2d2da3ff4e4d98741ed0fca87e32ea1bce944c7

--- infra/gerrit/apply_patchsets.sh
+++ infra/gerrit/apply_patchsets.sh
@@ -14,7 +14,7 @@ git config --get user.email >/dev/null 2>&1 || git config --global user.email "t
 cd $src_dir/$project_fqdn
 for ref in $(cat $patchsets_info_file | jq -r --arg project $project_fqdn '.[] | select(.project == $project) | .ref'); do
   echo "INFO: run 'git fetch $GERRIT_URL/$project_fqdn $ref'"
-  git fetch $GERRIT_URL/$project_fqdn $ref
+  git -c http.sslVerify=false fetch $GERRIT_URL/$project_fqdn $ref
   echo "INFO: FETCH_HEAD - $(git log -1 --oneline HEAD)"
   fetch_head_sha=$(git log -1 --oneline --no-abbrev-commit FETCH_HEAD | awk '{print $1}')
   if ! git log --oneline --no-abbrev-commit | grep $fetch_head_sha ; then
