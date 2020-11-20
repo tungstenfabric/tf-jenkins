c2933ba6cbd3a77509cf387b9c71315a937183bf
Sergey Baukin <baukin@gmail.com>
2020-11-21 00:30:53 +0300

    docker registry mirror configuration
    Change-Id: Icba100e08b74da34ecce11fc114fe4675526e76a

--- infra/hooks/ubuntu/up.sh
+++ infra/hooks/ubuntu/up.sh
@@ -13,9 +13,13 @@ source "${WORKSPACE}/deps.${JOB_NAME}.${JOB_RND}.env" || /bin/true
 source "${WORKSPACE}/vars.${JOB_NAME}.${JOB_RND}.env" || /bin/true
 
 rsync -a -e "ssh -i ${WORKER_SSH_KEY} ${SSH_OPTIONS}" "$my_dir/../../mirrors/mirror-pip.conf" ${IMAGE_SSH_USER}@${instance_ip}:./pip.conf
+rsync -a -e "ssh -i ${WORKER_SSH_KEY} ${SSH_OPTIONS}" "$my_dir/../../mirrors/mirror-docker-daemon.json" ${IMAGE_SSH_USER}@${instance_ip}:./docker-daemon.json
 
 cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $IMAGE_SSH_USER@$instance_ip
-#sudo cp -f ./pip.conf /etc/pip.conf
+sudo cp -f ./pip.conf /etc/pip.conf
+sudo mkdir -p /etc/docker/
+sudo cp -f ./docker-daemon.json /etc/docker/daemon.json
+sudo kill -SIGHUP $(pidof dockerd)
 echo "INFO: Update ubuntu OS"
 echo "APT::Acquire::Retries \"10\";" | sudo tee /etc/apt/apt.conf.d/80-retries
 sudo cp -f /usr/share/unattended-upgrades/20auto-upgrades-disabled /etc/apt/apt.conf.d/ || /bin/true
