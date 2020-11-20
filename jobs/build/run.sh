26681486163d249570981e8f18f2381c160187a5
Sergey Baukin <baukin@gmail.com>
2020-11-21 00:30:53 +0300

    docker registry mirror configuration
    Change-Id: Icba100e08b74da34ecce11fc114fe4675526e76a

--- jobs/build/run.sh
+++ jobs/build/run.sh
@@ -114,6 +114,11 @@ for mirror in $mirror_list ; do
 done
 cp \${WORKSPACE}/src/tungstenfabric/tf-jenkins/infra/mirrors/mirror-pip.conf ./config/etc/pip.conf
 
+sudo mkdir -p /etc/docker/
+sudo cp \${WORKSPACE}/src/tungstenfabric/tf-jenkins/infra/mirrors/mirror-docker-daemon.json /etc/docker/daemon.json
+#sudo kill -SIGHUP $(pidof dockerd)
+
+
 echo "INFO: df -h"
 df -h
 echo "INFO: free -h"
