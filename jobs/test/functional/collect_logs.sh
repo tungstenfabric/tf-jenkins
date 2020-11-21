e146d25cbf84fe6f4b9573a03ca497bef9716291
Andrey Pavlov <andrey-mp@yandex.ru>
2020-11-21 10:02:10 +0300

    add deployment test
    Depends-On: Ie598a2c0d511734d736ca9a5fedfa100218bf4f0
    Change-Id: Ie63499d938ec501d7732f7c28808b79d20cfc003

--- jobs/test/functional/collect_logs.sh
+++ jobs/test/functional/collect_logs.sh
@@ -8,17 +8,17 @@ my_dir="$(dirname $my_file)"
 source "$my_dir/definitions"
 
 ssh_cmd="ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $SSH_EXTRA_OPTIONS"
-SANITY_LOGS_PATH="contrail-test-runs"
 
 rm -rf $WORKSPACE/logs
 mkdir -p $WORKSPACE/logs
-testdir=$(eval $ssh_cmd $IMAGE_SSH_USER@$instance_ip ls -1 $SANITY_LOGS_PATH 2>/dev/null | sort | tail -1)
-rsync -a -e "$ssh_cmd" $IMAGE_SSH_USER@$instance_ip:$SANITY_LOGS_PATH/$testdir/reports/ $WORKSPACE/logs/ || /bin/true
-rsync -a -e "$ssh_cmd" $IMAGE_SSH_USER@$instance_ip:$SANITY_LOGS_PATH/$testdir/logs $WORKSPACE/logs/ || /bin/true
+pushd $WORKSPACE/logs
+rsync -a -e "$ssh_cmd" $IMAGE_SSH_USER@$instance_ip:logs.tgz . || /bin/true
+tar -xvf logs.tgz
+popd
 
 FULL_LOGS_PATH="${LOGS_PATH}/${JOB_LOGS_PATH}/sanity"
 ssh -i $LOGS_HOST_SSH_KEY $SSH_OPTIONS $LOGS_HOST_USERNAME@$LOGS_HOST "mkdir -p $FULL_LOGS_PATH"
 rsync -a -e "ssh -i $LOGS_HOST_SSH_KEY $SSH_OPTIONS" $WORKSPACE/logs/ $LOGS_HOST_USERNAME@$LOGS_HOST:$FULL_LOGS_PATH
 rm -rf $WORKSPACE/logs
 
-echo "INFO: Sanity logs saved"
+echo "INFO: logs saved"
