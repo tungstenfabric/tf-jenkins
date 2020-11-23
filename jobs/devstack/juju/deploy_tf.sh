5c374c508578f00340033df45f3427f0aba9160b
Andrey Pavlov <andrey-mp@yandex.ru>
2020-11-23 15:08:19 +0300

    save correct devstack profile in logs stage
    Change-Id: I9991509850d6698b99a16a42440226f0425f50ce

--- jobs/devstack/juju/deploy_tf.sh
+++ jobs/devstack/juju/deploy_tf.sh
@@ -8,16 +8,4 @@ my_dir="$(dirname $my_file)"
 
 source "$my_dir/definitions"
 
-function add_deployrc() {
-  local file="$1"
-  cat <<EOF >> "$file"
-export CLOUD=$CLOUD
-source \$HOME/$CLOUD.vars || /bin/true
-export JUJU_CONTROLLER_NODES="$JUJU_CONTROLLER_NODES"
-export CONTROL_NETWORK="$CONTROL_NETWORK"
-export DATA_NETWORK="$DATA_NETWORK"
-EOF
-}
-export -f add_deployrc
-
 ${my_dir}/../common/deploy_tf.sh juju
--- jobs/devstack/openshift/deploy_tf.sh
+++ jobs/devstack/openshift/deploy_tf.sh
@@ -8,13 +8,4 @@ my_dir="$(dirname $my_file)"
 
 source "$my_dir/definitions"
 
-function add_deployrc() {
-  local file="$1"
-  cat <<EOF >> "$file"
-export RHEL_OPENSHIFT_REGISTRY=$RHEL_OPENSHIFT_REGISTRY
-sudo setenforce 0
-EOF
-}
-export -f add_deployrc
-
 ${my_dir}/../common/deploy_tf.sh openshift
