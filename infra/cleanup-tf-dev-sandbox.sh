9818cc0c525d5908b36044c092c7c9f7e736a24b
Andrey Pavlov <andrey-mp@yandex.ru>
2020-12-16 17:07:42 +0300

    fix typo
    closes-jira-bug: CEM-20451
    Change-Id: I7f6a74e5e08853a7f225cd6fe05cb53ee949a4b5

--- infra/cleanup-tf-dev-sandbox.sh
+++ infra/cleanup-tf-dev-sandbox.sh
@@ -4,7 +4,7 @@ nexus_rest_url="http://tf-nexus.progmaticlab.com/service/rest/v1"
 registries="tungsten_ci tungsten_gate_cache"
 image_name="tf-dev-sandbox"
 
-for registry in registries ; do
+for registry in $registries ; do
   echo "INFO: registry = ${registry}"
   for id in $(curl -s "${nexus_rest_url}/search?repository=${registry}&name=${image_name}" | jq -r .items[].id) ; do
     echo "INFO: id = $id"
