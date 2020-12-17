88034bfef45463357a5ae7472a3b3960f373c401
shrike <shrike_@mail.ru>
2020-12-17 08:23:41 +0000

    enable debug in check_approvals
    it is required to investigate a not starting gate
    
    Change-Id: I3e2cb8ff9e9970603bda032019c28bd0707c6ca3

--- pipelines/utils/gerrit.groovy
+++ pipelines/utils/gerrit.groovy
@@ -337,6 +337,7 @@ def _has_approvals(strategy) {
     try {
       output = sh(returnStdout: true, script: """
         ${WORKSPACE}/src/tungstenfabric/tf-jenkins/infra/gerrit/check_approvals.py \
+          --debug \
           --strategy ${strategy} \
           --gerrit ${url} \
           --user ${GERRIT_API_USER} \
