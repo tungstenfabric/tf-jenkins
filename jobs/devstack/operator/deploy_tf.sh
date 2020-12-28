7022489777e280fa3fa7697e925143f06c38908e
tikitavi <rtikitavi@gmail.com>
2020-12-28 15:28:00 +0300

    add operator to templates
    Change-Id: Ic8b2b86e7fb765d8d940525b57b1d1b75e20c7ec
    Signed-off-by: tikitavi <rtikitavi@gmail.com>
    Partial-Jira-bug: CEM-20905

---
+++ jobs/devstack/operator/deploy_tf.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+set -o pipefail
+
+[ "${DEBUG,,}" == "true" ] && set -x
+
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname $my_file)"
+
+source "$my_dir/definitions"
+
+${my_dir}/../common/deploy_tf.sh operator
