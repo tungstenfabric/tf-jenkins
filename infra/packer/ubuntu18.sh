85ad6bfee3335c85df5768366f064f3e4172a8f0
shrike <shrike_@mail.ru>
2021-01-27 13:59:34 +0000

    enable kdump
    we need it to collect a crash if the kernel oops. required tools
    are inside linux-crashdump package. install it and enable crash
    collection.
    
    Change-Id: I795a522cbce2335821ece48b3c0de93e1e5dc823

--- infra/packer/ubuntu18.sh
+++ infra/packer/ubuntu18.sh
@@ -5,3 +5,7 @@ sudo systemctl disable --now apt-daily{,-upgrade}.{timer,service}
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo chsh --shell /bin/bash ubuntu
+echo "kexec-tools kexec-tools/load_kexec boolean true" | sudo debconf-set-selections
+echo "kdump-tools kdump-tools/use_kdump boolean true" | sudo debconf-set-selections
+DEBIAN_FRONTEND=noninteractive sudo apt install linux-crashdump -fy --fix-missing
+
