43e0690741ef7b2496545a434d7f2c07e4688271
Andrey Pavlov <andrey-mp@yandex.ru>
2021-03-06 14:47:35 +0300

    add contrail-third-party and tpc to update-static-cache job
    Change-Id: I9e371e6f4b23d4dfe6bd4dafe2976329ca185ad9

--- jobs/update-static-cache/run.sh
+++ jobs/update-static-cache/run.sh
@@ -8,9 +8,9 @@ my_dir="$(dirname $my_file)"
 
 source "$my_dir/definitions"
 
-rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS" {$WORKSPACE/src,$my_dir/update_external_web_cache.sh} $IMAGE_SSH_USER@$instance_ip:./
+rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS" {$WORKSPACE/src,$my_dir/update_static_cache.sh} $IMAGE_SSH_USER@$instance_ip:./
 
-echo "INFO: Update external web cache started"
+echo "INFO: Update caches started"
 cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $IMAGE_SSH_USER@$instance_ip
 #!/bin/bash -e
 [ "${DEBUG,,}" == "true" ] && set -x
@@ -18,10 +18,9 @@ export WORKSPACE=\$HOME
 export DEBUG=$DEBUG
 export TPC_REPO_USER=$TPC_REPO_USER
 export TPC_REPO_PASS=$TPC_REPO_PASS
+export REPO_SOURCE=http://tf-nexus.progmaticlab.com/repository
 
-export EXTERNAL_WEB_CACHE_REPO=http://tf-nexus.progmaticlab.com/repository/external-web-cache
-export CACHE_DIR='external-web-cache'
-
-./update_external_web_cache.sh
+./update_static_cache.sh
 EOF
-echo "INFO: Update external web cache finished"
+
+echo "INFO: Update caches finished"
