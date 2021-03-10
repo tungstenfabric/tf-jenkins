75c6653c6414de363f0d64e59d0b5b3a34bdac29
Andrey Pavlov <andrey-mp@yandex.ru>
2021-03-10 12:56:27 +0300

    do not upload images.tgz in update-sanity-images
    Change-Id: Iac22223de29ef042f32706e09732e66f6d5e672b

--- jobs/update-nexus/update_sanity_images.sh
+++ jobs/update-nexus/update_sanity_images.sh
@@ -17,6 +17,7 @@ pushd sanity_images
 # this archive already has 'images' prefix in file's path
 wget -nv https://object-storage.public.mtl1.vexxhost.net/swift/v1/558a8ca6c0484c09b4dc140698842c7a/tf-ci/images.tgz
 tar -xvf images.tgz
+rm images.tgz
 for file in $(find . -type f) ; do
   echo "INFO: upload $file"
   curl -s --user "${TPC_REPO_USER}:${TPC_REPO_PASS}" --ftp-create-dirs -T $file $REPO_SOURCE/$file
