c88d33e84c4fed6432254c505ba5d23db3d485f6
Andrey Pavlov <andrey-mp@yandex.ru>
2021-03-13 23:55:10 +0300

    remove outdated files
    Change-Id: Ia55108efe723ac4584a9d7022fe9517c9bc5c2d8

--- infra/mirrors/rhel7/update_repo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-my_file="$(readlink -e "$0")"
-my_dir="$(dirname $my_file)"
-
-source $my_dir/common.sh
-
-repos="rhel-7-server-rpms rhel-7-server-extras-rpms rhel-7-server-optional-rpms rhel-server-rhscl-7-rpms rhel-ha-for-rhel-7-server-rpms rhel-7-server-rhceph-3-tools-rpms"
-update_repos $repos
--- infra/mirrors/rhel7/update_repo_openstack.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-my_file="$(readlink -e "$0")"
-my_dir="$(dirname $my_file)"
-
-source $my_dir/common.sh
-
-repos="rhel-7-server-rh-common-rpms rhel-ha-for-rhel-7-server-rpms rhel-7-server-openstack-13-devtools-rpms rhel-7-server-openstack-13-rpms"
-update_repos $repos
