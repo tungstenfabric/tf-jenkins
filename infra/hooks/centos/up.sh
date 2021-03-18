5cab4d95d806254bd3f7b692db2c9b6879b48471
Gleb Galkin <gleb108@gmail.com>
2021-03-18 13:39:56 +0300

    Adding REPOS_CHANNEL switching to centos hook
    Signed-off-by: Gleb Galkin <gleb108@gmail.com>
    Change-Id: Icb3da31b8805655b2e2b816b7417b8851317b50e

--- infra/hooks/centos/up.sh
+++ infra/hooks/centos/up.sh
@@ -15,7 +15,6 @@ source "${WORKSPACE}/vars.${JOB_NAME}.${JOB_RND}.env" || /bin/true
 rsync -a -e "ssh -i ${WORKER_SSH_KEY} ${SSH_OPTIONS}" "$my_dir/../../mirrors/mirror-pip.conf" ${IMAGE_SSH_USER}@${instance_ip}:./pip.conf
 rsync -a -e "ssh -i ${WORKER_SSH_KEY} ${SSH_OPTIONS}" "$my_dir/../../mirrors/mirror-docker-daemon.json" ${IMAGE_SSH_USER}@${instance_ip}:./docker-daemon.json
 
-
 cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $IMAGE_SSH_USER@$instance_ip
 sudo cp -f ./pip.conf /etc/pip.conf
 sudo mkdir -p /etc/docker/
@@ -27,6 +26,11 @@ if [ -f $my_dir/../../mirrors/centos7-environment ]; then
   echo "INFO: copy additional environment to host"
   rsync -a -e "ssh -i ${WORKER_SSH_KEY} ${SSH_OPTIONS}" "$my_dir/../../mirrors/centos7-environment" ${IMAGE_SSH_USER}@${instance_ip}:./centos7-environment
   ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $IMAGE_SSH_USER@$instance_ip "cat centos7-environment | sudo tee -a /etc/environment"
+fi
+
+# Switch REPOS_CHANNEL
+if [[ -n "$REPOS_CHANNEL" && "$REPOS_CHANNEL" != 'latest' ]]; then
+  sed -i "s|/latest/|/${REPOS_CHANNEL}/|g" $my_dir/../../mirrors/mirror-base.repo
 fi
 
 if [ -f $my_dir/../../mirrors/mirror-base.repo ]; then
