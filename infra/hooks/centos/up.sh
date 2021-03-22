31d91b50781a4af9dda458fb344a8eba862cc336
Sergey Baukin <baukin@gmail.com>
2021-03-22 19:40:23 +0300

    update packer centos7 resolv.conf

--- infra/hooks/centos/up.sh
+++ infra/hooks/centos/up.sh
@@ -14,9 +14,11 @@ source "${WORKSPACE}/vars.${JOB_NAME}.${JOB_RND}.env" || /bin/true
 
 rsync -a -e "ssh -i ${WORKER_SSH_KEY} ${SSH_OPTIONS}" "$my_dir/../../mirrors/mirror-pip.conf" ${IMAGE_SSH_USER}@${instance_ip}:./pip.conf
 rsync -a -e "ssh -i ${WORKER_SSH_KEY} ${SSH_OPTIONS}" "$my_dir/../../mirrors/mirror-docker-daemon.json" ${IMAGE_SSH_USER}@${instance_ip}:./docker-daemon.json
+rsync -a -e "ssh -i ${WORKER_SSH_KEY} ${SSH_OPTIONS}" "$my_dir/resolv.conf" ${IMAGE_SSH_USER}@${instance_ip}:./resolv.conf
 
 cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $IMAGE_SSH_USER@$instance_ip
 sudo cp -f ./pip.conf /etc/pip.conf
+sudo cp -f ./resolv.conf /etc/resolv.conf
 sudo mkdir -p /etc/docker/
 sudo cp -f ./docker-daemon.json /etc/docker/daemon.json
 #sudo kill -SIGHUP $(pidof dockerd)
