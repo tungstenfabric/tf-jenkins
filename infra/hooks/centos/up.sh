f53a08cb4488496388f9c83f15df470bdef9abee
Arina Argentova <aargentova@progmaticlab.com>
2021-04-07 02:38:15 +0300

    fix routing in centos when USE_DATAPLANE_NETWORK is true
    Signed-off-by: aargentova@progmaticlab.com
    Change-Id: I50235825f76d4abf7ffab7d534761707566e2bc9

--- infra/hooks/centos/up.sh
+++ infra/hooks/centos/up.sh
@@ -12,12 +12,14 @@ source "$WORKSPACE/stackrc.$JOB_NAME.env" || /bin/true
 source "${WORKSPACE}/deps.${JOB_NAME}.${JOB_RND}.env" || /bin/true
 source "${WORKSPACE}/vars.${JOB_NAME}.${JOB_RND}.env" || /bin/true
 
-cat $my_dir/../../mirrors/mirror-pip.conf | envsubst > "$WORKSPACE/mirror-pip.conf"
-rsync -a -e "ssh -i ${WORKER_SSH_KEY} ${SSH_OPTIONS}" "$WORKSPACE/mirror-pip.conf" ${IMAGE_SSH_USER}@${instance_ip}:./pip.conf
-cat $my_dir/../../mirrors/mirror-docker-daemon.json | envsubst > "$WORKSPACE/mirror-docker-daemon.json"
-rsync -a -e "ssh -i ${WORKER_SSH_KEY} ${SSH_OPTIONS}" "$WORKSPACE/mirror-docker-daemon.json" ${IMAGE_SSH_USER}@${instance_ip}:./docker-daemon.json
+ssh_cmd="ssh -i ${WORKER_SSH_KEY} ${SSH_OPTIONS}"
 
-cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $IMAGE_SSH_USER@$instance_ip
+cat $my_dir/../../mirrors/mirror-pip.conf | envsubst > "$WORKSPACE/mirror-pip.conf"
+rsync -a -e "$ssh_cmd" "$WORKSPACE/mirror-pip.conf" ${IMAGE_SSH_USER}@${instance_ip}:./pip.conf
+cat $my_dir/../../mirrors/mirror-docker-daemon.json | envsubst > "$WORKSPACE/mirror-docker-daemon.json"
+rsync -a -e "$ssh_cmd" "$WORKSPACE/mirror-docker-daemon.json" ${IMAGE_SSH_USER}@${instance_ip}:./docker-daemon.json
+
+cat <<EOF | $ssh_cmd $IMAGE_SSH_USER@$instance_ip
 sudo cp -f ./pip.conf /etc/pip.conf
 sudo mkdir -p /etc/docker/
 sudo cp -f ./docker-daemon.json /etc/docker/daemon.json
@@ -27,11 +29,17 @@ EOF
 if [ -f $my_dir/../../mirrors/centos7-environment ]; then
   echo "INFO: copy additional environment to host"
   cat $my_dir/../../mirrors/centos7-environment | envsubst > "$WORKSPACE/centos7-environment"
-  rsync -a -e "ssh -i ${WORKER_SSH_KEY} ${SSH_OPTIONS}" "$WORKSPACE/centos7-environment" ${IMAGE_SSH_USER}@${instance_ip}:./centos7-environment
-  ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $IMAGE_SSH_USER@$instance_ip "cat centos7-environment | sudo tee -a /etc/environment"
+  rsync -a -e "$ssh_cmd" "$WORKSPACE/centos7-environment" ${IMAGE_SSH_USER}@${instance_ip}:./centos7-environment
+  $ssh_cmd $IMAGE_SSH_USER@$instance_ip "cat centos7-environment | sudo tee -a /etc/environment"
 fi
 
 echo "INFO: copy mirror-base.repo to host"
 cat $my_dir/../../mirrors/mirror-base.repo | envsubst > "$WORKSPACE/mirror-base.repo"
-rsync -a -e "ssh -i ${WORKER_SSH_KEY} ${SSH_OPTIONS}" "$WORKSPACE/mirror-base.repo" ${IMAGE_SSH_USER}@${instance_ip}:./mirror-base.repo
-ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $IMAGE_SSH_USER@$instance_ip "sudo rm -f /etc/yum.repos.d/*; sudo cp mirror-base.repo /etc/yum.repos.d/"
+rsync -a -e "$ssh_cmd" "$WORKSPACE/mirror-base.repo" ${IMAGE_SSH_USER}@${instance_ip}:./mirror-base.repo
+$ssh_cmd $IMAGE_SSH_USER@$instance_ip "sudo rm -f /etc/yum.repos.d/*; sudo cp mirror-base.repo /etc/yum.repos.d/"
+
+# TODO: detect interface name
+echo "INFO: do not set default gateway for second interface"
+if [[ "${USE_DATAPLANE_NETWORK,,}" == "true" ]]; then
+  $ssh_cmd $IMAGE_SSH_USER@$instance_ip "echo 'DEFROUTE=no' >> /etc/sysconfig/network-scripts/ifcfg-eth1 ; sudo systemctl restart network.service ; sudo dhclient -r && sudo dhclient"
+fi
