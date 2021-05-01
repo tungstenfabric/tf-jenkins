1fc5c029fed06039750483fd45e5f14c97c7f08d
Andrey Pavlov <andrey-mp@yandex.ru>
2021-05-01 09:44:25 +0300

    do not fail update-images if retain is failed
    Change-Id: I52f1cf3cd64deeaab900e98ae7f639bbdf9e1e99

--- jobs/update-stage-images/publish_docker_images.sh
+++ jobs/update-stage-images/publish_docker_images.sh
@@ -4,8 +4,8 @@ my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
 if [[ -z ${REPOS_TYPE} ]]; then
-   echo "REPOS_TYPE is not defined. Exit"
-   exit 1
+  echo "REPOS_TYPE is not defined. Exit"
+  exit 1
 fi
 
 [ -f $my_dir/${REPOS_TYPE}.env ] || exit 1
@@ -36,15 +36,15 @@ res=0
 
 for c in ${all_stable_images} ; do
   echo "INFO: start retain $c"
-  tag_container $c $REDHAT_TAG $RETAIN_TAG || res=1
+  tag_container $c $REDHAT_TAG $RETAIN_TAG || /bin/true
 done
 
 if [[ -n "$UBI_REDHAT_REGISTRY" && "$UBI_NAMESPACE" && "$UBI_STABLE_TAG" && $UBI_STAGE_TAG ]] ; then
-   ubi_stable_images+=$(printf "${UBI_NAMESPACE}/%s:$UBI_STABLE_TAG " "${ubi_images[@]}")
-   for c in ${ubi_stable_images} ; do
-      echo "INFO: ubi start retain $c"
-      tag_container $c $UBI_STABLE_TAG $UBI_RETAIN_TAG || res=1
-   done
+  ubi_stable_images+=$(printf "${UBI_NAMESPACE}/%s:$UBI_STABLE_TAG " "${ubi_images[@]}")
+  for c in ${ubi_stable_images} ; do
+    echo "INFO: ubi start retain $c"
+    tag_container $c $UBI_STABLE_TAG $UBI_RETAIN_TAG || /bin/true
+  done
 fi
 
 echo "INFO: retaining succeeded"
@@ -55,11 +55,11 @@ for c in ${all_stage_images} ; do
 done
 
 if [[ -n "$UBI_REDHAT_REGISTRY" && "$UBI_NAMESPACE" && "$UBI_STABLE_TAG" && $UBI_STAGE_TAG ]] ; then
-   ubi_stage_images+=$(printf "${UBI_NAMESPACE}/%s:$UBI_STAGE_TAG " "${ubi_images[@]}")
-   for c in ${ubi_stage_images} ; do
-      echo "INFO: ubi start publish $c"
-      tag_container $c $UBI_STAGE_TAG $UBI_STABLE_TAG || res=1
-   done
+  ubi_stage_images+=$(printf "${UBI_NAMESPACE}/%s:$UBI_STAGE_TAG " "${ubi_images[@]}")
+  for c in ${ubi_stage_images} ; do
+    echo "INFO: ubi start publish $c"
+    tag_container $c $UBI_STAGE_TAG $UBI_STABLE_TAG || res=1
+  done
 fi
 
 if [[ $res != 0 ]] ; then
