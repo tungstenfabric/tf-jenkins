a2c9c89ed3aeb4e221ea09d6471455b52c7f316f
Andrey Pavlov <andrey-mp@yandex.ru>
2021-06-08 20:38:38 +0300

    fix path and ssh dest in update-stage-repos
    Change-Id: I880b93b27998b3db8405b465e75cf0812352973e

--- jobs/update-stage-repos/publish.sh
+++ jobs/update-stage-repos/publish.sh
@@ -10,10 +10,11 @@ if [ -z ${REPOS_TYPE} ]; then
   exit 1
 fi
 
-rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS" $WORKSPACE/src $IMAGE_SSH_USER@$instance_ip:./
+dest="$REPOUPDATER_USER_NAME@tf-mirrors.$SLAVE_REGION.$CI_DOMAIN"
+rsync -a -e "ssh -i $WORKER_SSH_KEY $SSH_OPTIONS" $WORKSPACE/src $dest:./
 
-cat <<EOF | ssh -i $REPOUPDATER_SSH_KEY $SSH_OPTIONS $REPOUPDATER_USER_NAME@tf-mirrors.$SLAVE_REGION.$CI_DOMAIN
+cat <<EOF | ssh -i $REPOUPDATER_SSH_KEY $SSH_OPTIONS $dest
 export SLAVE_REGION=$SLAVE_REGION
 export CI_DOMAIN=$CI_DOMAIN
-./src/tungstenfabric/tf-jenkins/jobs/update-stage-images/publish_stage.sh ${REPOS_TYPE}
+./src/tungstenfabric/tf-jenkins/jobs/update-stage-repos/publish_stage.sh ${REPOS_TYPE}
 EOF
