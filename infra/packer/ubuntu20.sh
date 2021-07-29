cda7aa456ddbdb56c702275c1991a21663b272c7
Arina Argentova <aargentova@progmaticlab.com>
2021-07-29 16:54:57 +0300

    remove logging for infra/packer/ubuntu20.sh
    Signed-off-by: aargentova@progmaticlab.com
    Change-Id: I95fc96f2e975413ce6519ecc19f8f817e680b974

--- infra/packer/ubuntu20.sh
+++ infra/packer/ubuntu20.sh
@@ -1,14 +1,10 @@
 #!/bin/bash -eE
 set -o pipefail
 
-sudo cat /etc/apt/sources.list
-
 sudo systemctl disable --now apt-daily{,-upgrade}.{timer,service}
 sudo apt update -y
 sudo apt upgrade -y
 sudo chsh --shell /bin/bash ubuntu
-sudo cat /etc/apt/sources.list
 echo "kexec-tools kexec-tools/load_kexec boolean true" | sudo debconf-set-selections
 echo "kdump-tools kdump-tools/use_kdump boolean true" | sudo debconf-set-selections
-sudo apt-cache search net-tools
 DEBIAN_FRONTEND=noninteractive sudo apt install linux-crashdump net-tools -fy --fix-missing
