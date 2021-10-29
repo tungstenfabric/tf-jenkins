d8a8748074bebb5248656ac82a080684d5de7cbd
Gleb Galkin <gleb108@gmail.com>
2021-10-29 14:06:13 +0300

    Fix local repos in rhel hook
    Signed-off-by: Gleb Galkin <gleb108@gmail.com>
    Change-Id: I4066002031adc74b0eff3b5e9f27e7fcbdea95ca

--- infra/hooks/rhel/up.sh
+++ infra/hooks/rhel/up.sh
@@ -30,20 +30,20 @@ EOF
 
 
 # TODO: not clear.. it looks it is not needed as images use mirrors 
-#
-# repofile="$my_dir/../../mirrors/mirror-${ENVIRONMENT_OS}.repo"
-# if [[ -f $repofile ]]; then
-#     echo "INFO: Using repofile $repofile"
-# else
-#     echo "repofile $repofile not found. Stop"
-#     exit 1
-# fi
 
-# cat $repofile | envsubst > $WORKSPACE/local.repo
-# rsync -a -e "ssh -i ${WORKER_SSH_KEY} ${SSH_OPTIONS}" "$WORKSPACE/local.repo" ${IMAGE_SSH_USER}@${instance_ip}:./local.repo
-# rsync -a -e "ssh -i ${WORKER_SSH_KEY} ${SSH_OPTIONS}" "$my_dir/distribute_local_repos.sh" ${IMAGE_SSH_USER}@${instance_ip}:./distribute_local_repos.sh
+repofile="$my_dir/../../mirrors/mirror-${ENVIRONMENT_OS}.repo"
+if [[ -f $repofile ]]; then
+    echo "INFO: Using repofile $repofile"
+else
+    echo "repofile $repofile not found. Stop"
+    exit 1
+fi
 
-# cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $IMAGE_SSH_USER@$instance_ip
-# sudo cp -f ./local.repo /etc/yum.repos.d/local.repo
-# ./distribute_local_repos.sh
-# EOF
+cat $repofile | envsubst > $WORKSPACE/local.repo
+rsync -a -e "ssh -i ${WORKER_SSH_KEY} ${SSH_OPTIONS}" "$WORKSPACE/local.repo" ${IMAGE_SSH_USER}@${instance_ip}:./local.repo
+rsync -a -e "ssh -i ${WORKER_SSH_KEY} ${SSH_OPTIONS}" "$my_dir/distribute_local_repos.sh" ${IMAGE_SSH_USER}@${instance_ip}:./distribute_local_repos.sh
+
+cat <<EOF | ssh -i $WORKER_SSH_KEY $SSH_OPTIONS $IMAGE_SSH_USER@$instance_ip
+sudo cp -f ./local.repo /etc/yum.repos.d/local.repo
+./distribute_local_repos.sh
+EOF
