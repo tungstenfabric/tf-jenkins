c2f8b59670d70594ec7c9b7360df846a0c0c3f53
Alexey <alexey.morlang@gmail.com>
2021-11-16 10:52:38 +0300

    fixed centos8 sync script
    Change-Id: I3495b22c4f477ecbd34bf5f1492f503c1cf28134

--- setup/playbooks/roles/mirrors/files/centos8/syncrepos.sh
+++ setup/playbooks/roles/mirrors/files/centos8/syncrepos.sh
@@ -17,13 +17,28 @@ for repo in "centos8" "yum8" ; do
   fi
 done
 
+function retry() {
+  local i
+  for ((i=0; i<5; ++i)) ; do
+    if $@ ; then
+      break
+    fi
+    echo "COMMAND FAILED: $@"
+    echo "RETRYING COMMAND (time=$i out of 5)"
+  done
+  if [[ $i == 5 ]]; then
+    echo "ERROR: COMMAND FAILED AFTER 5 tries: $@"
+    exit 1
+  fi
+}
+
 for r in ${REPOS_CENTOS[@]}; do
-  reposync -l --repoid=${r} --download-metadata --downloadcomps --download_path=${MIRRORDIR}/centos8/${DATE}
+  retry reposync --repoid=${r} --download-metadata --downloadcomps --download-path=${MIRRORDIR}/centos8/${DATE}
   createrepo -v ${MIRRORDIR}/centos8/${DATE}/${r}/
 done
 
 for r in ${REPOS_YUM[@]}; do
-  reposync -l --repoid=${r} --download-metadata --downloadcomps --download_path=${MIRRORDIR}/yum8/${DATE}
+  retry reposync --repoid=${r} --download-metadata --downloadcomps --download-path=${MIRRORDIR}/yum8/${DATE}
   createrepo -v ${MIRRORDIR}/yum8/${DATE}/${r}/
 done
 
