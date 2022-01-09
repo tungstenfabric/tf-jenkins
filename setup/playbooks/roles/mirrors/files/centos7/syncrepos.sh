32c1de4b5ddc9874f34689a3fe74df79a704a17a
Andrey Pavlov <andrey-mp@yandex.ru>
2022-01-09 16:46:21 +0300

    add SCLo repos to stage list
    Change-Id: Idee77ae8d5255af7d29870409bc900a9ba5f12ee

--- setup/playbooks/roles/mirrors/files/centos7/syncrepos.sh
+++ setup/playbooks/roles/mirrors/files/centos7/syncrepos.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -e
 
-REPOS_CENTOS7=(base centosplus extras updates)
+REPOS_CENTOS7=(base centosplus extras updates centos-sclo-rh)
 REPOS_YUM7=(centos-openstack-queens dockerrepo epel k8s)
 MIRRORDIR=/repos
 DATE=$(date +"%Y%m%d")
 
+echo "INFO: preparing temp folders for downloading"
 for repo in "centos7" "yum7" ; do
   if [ ! -d ${MIRRORDIR}/$repo/${DATE} ]; then
     mkdir -p ${MIRRORDIR}/$repo/${DATE}
@@ -17,15 +18,18 @@ for repo in "centos7" "yum7" ; do
 done
 
 for r in ${REPOS_CENTOS7[@]}; do
+  echo "INFO: updating centos7 repoid=$r"
   reposync -l --repoid=${r} --download-metadata --downloadcomps --download_path=${MIRRORDIR}/centos7/${DATE}
   createrepo -v ${MIRRORDIR}/centos7/${DATE}/${r}/
 done
 
 for r in ${REPOS_YUM7[@]}; do
+  echo "INFO: updating yum7 repoid=$r"
   reposync -l --repoid=${r} --download-metadata --downloadcomps --download_path=${MIRRORDIR}/yum7/${DATE}
   createrepo -v ${MIRRORDIR}/yum7/${DATE}/${r}/
 done
 
+echo "INFO: switching dowloaded repos to stage"
 for repo in "centos7" "yum7" ; do
   pushd ${MIRRORDIR}/$repo
   rm -f stage
