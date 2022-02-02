9505c8fc6fbef48bfc0433c4e22ec7eac7a5b1bd
Alexey <alexey.morlang@gmail.com>
2022-02-02 19:10:35 +0300

    added kernel 3.10.0-1160.53.1.el7.x86_64 for tpc cache
    Change-Id: Ia0bdf62b05c53693f756e8d079cd2e5f96e2fab8

--- jobs/update-caches/update_third_party_packages.sh
+++ jobs/update-caches/update_third_party_packages.sh
@@ -87,6 +87,8 @@ kernels="
 
 # current packages - should be taken from archive after new release
 kernels+="
+  http://mirror.centos.org/centos/7/updates/x86_64/Packages/kernel-3.10.0-1160.53.1.el7.x86_64.rpm
+  http://mirror.centos.org/centos/7/updates/x86_64/Packages/kernel-devel-3.10.0-1160.53.1.el7.x86_64.rpm
   http://mirror.centos.org/centos/7/updates/x86_64/Packages/kernel-3.10.0-1160.25.1.el7.x86_64.rpm
   http://mirror.centos.org/centos/7/updates/x86_64/Packages/kernel-devel-3.10.0-1160.25.1.el7.x86_64.rpm
   https://vault.centos.org/8.4.2105/BaseOS/x86_64/os/Packages/kernel-4.18.0-305.12.1.el8_4.x86_64.rpm
