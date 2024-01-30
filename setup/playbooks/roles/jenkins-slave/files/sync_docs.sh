aff7926dfee106714ad3f0c95f91dd085f3e8f26
tikitavi <rtikitavi@gmail.com>
2024-01-30 19:14:37 +0300

    add setup of syncing docs to aws s3
    Change-Id: Ibd70c2ab0774055995fe4de935a591c80bc1f8d0
    Signed-off-by: tikitavi <rtikitavi@gmail.com>

---
+++ setup/playbooks/roles/jenkins-slave/files/sync_docs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -ex
+date
+cd /docs
+if ! git pull | grep "Already up to date." ; then
+  /usr/bin/tox -e docs
+  cd _build/html
+  aws s3 sync . s3://docs.opensdn.io --delete --profile osdnwebsite
+fi
