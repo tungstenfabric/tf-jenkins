a3be34cf18d03d0244a3f79e82b55af4417f9682
tikitavi <rtikitavi@gmail.com>
2024-02-15 12:43:14 +0300

    fix for sync_docs
    Change-Id: I57ee0ad33dea4b64e3f409d7b32a42af3c0b3c9f
    Signed-off-by: tikitavi <rtikitavi@gmail.com>

--- setup/playbooks/roles/jenkins-slave/files/sync_docs.sh
+++ setup/playbooks/roles/jenkins-slave/files/sync_docs.sh
@@ -1,8 +1,9 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 date
 cd /docs
 if ! git pull | grep "Already up to date." ; then
   /usr/bin/tox -e docs
   cd _build/html
   aws s3 sync . s3://docs.opensdn.io --delete --profile osdnwebsite
+  "Successfully synced"
 fi
