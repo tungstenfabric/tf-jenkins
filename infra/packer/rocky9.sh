1dc88053cee1726c881da6c62bfd041872108278
Andrey Pavlov <andrey-mp@yandex.ru>
2024-02-25 11:25:09 +0300

    tune packer for rocky9
    don't update OS - don't update kernel to latest
    
    Change-Id: Ie73bcf114c0210636dad77007dc9903182b211bd

--- infra/packer/rocky9.sh
+++ infra/packer/rocky9.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eE
 set -o pipefail
 
-sudo yum update -y
-sudo yum install -y bind bind-utils haproxy httpd
+# NOTE: do not run dnf update - it ups kernel version which is not suported !
 
+sudo dnf install -y bind bind-utils haproxy httpd net-tools
